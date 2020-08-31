cf9a1f16a5d5fa2f3e01e8ccc9a7f6cc82aec36c
nitko12 <nitko12@hotmail.com>
2020-08-31 13:16:27 +0200

    Added deep sleep touch pad wake up.

--- Inkplate.cpp
+++ Inkplate.cpp
@@ -37,7 +37,7 @@ void Inkplate::begin(void)
     if (_beginDone == 1)
         return;
     Wire.begin();
-	mcpBegin(MCP23017_ADDR, mcpRegsInt);
+    mcpBegin(MCP23017_ADDR, mcpRegsInt);
     pinModeMCP(VCOM, OUTPUT);
     pinModeMCP(PWRUP, OUTPUT);
     pinModeMCP(WAKEUP, OUTPUT);
@@ -813,27 +813,29 @@ void Inkplate::cleanFast(uint8_t c, uint8_t rep)
         data = B11111111; //Skip
     }
 
-	uint32_t _send = ((data & B00000011) << 4) | (((data & B00001100) >> 2) << 18) | (((data & B00010000) >> 4) << 23) | (((data & B11100000) >> 5) << 25);;
-	for (int k = 0; k < rep; k++) {
-    vscan_start();
-    for (int i = 0; i < 600; i++)
-	{
-		hscan_start(_send);
-		GPIO.out_w1ts = (_send) | CL;
-		GPIO.out_w1tc = DATA | CL;
-		for (int j = 0; j < 99; j++)
-		{
-			GPIO.out_w1ts = (_send) | CL;
-			GPIO.out_w1tc = DATA | CL;
-			GPIO.out_w1ts = (_send) | CL;
-			GPIO.out_w1tc = DATA | CL;
-		}
-		GPIO.out_w1ts = (_send) | CL;
-		GPIO.out_w1tc = DATA | CL;
-		vscan_end();
+    uint32_t _send = ((data & B00000011) << 4) | (((data & B00001100) >> 2) << 18) | (((data & B00010000) >> 4) << 23) | (((data & B11100000) >> 5) << 25);
+    ;
+    for (int k = 0; k < rep; k++)
+    {
+        vscan_start();
+        for (int i = 0; i < 600; i++)
+        {
+            hscan_start(_send);
+            GPIO.out_w1ts = (_send) | CL;
+            GPIO.out_w1tc = DATA | CL;
+            for (int j = 0; j < 99; j++)
+            {
+                GPIO.out_w1ts = (_send) | CL;
+                GPIO.out_w1tc = DATA | CL;
+                GPIO.out_w1ts = (_send) | CL;
+                GPIO.out_w1tc = DATA | CL;
+            }
+            GPIO.out_w1ts = (_send) | CL;
+            GPIO.out_w1tc = DATA | CL;
+            vscan_end();
+        }
+        delayMicroseconds(230);
     }
-    delayMicroseconds(230);
-  }
 }
 
 void Inkplate::cleanFast2(uint8_t c, uint8_t n, uint16_t d)
@@ -917,122 +919,123 @@ void Inkplate::pinsAsOutputs()
 }
 
 //----------------------------MCP23017 Functions----------------------------
-void Inkplate::pinModeMCP(uint8_t _pin, uint8_t _mode) 
+void Inkplate::pinModeMCP(uint8_t _pin, uint8_t _mode)
 {
-	uint8_t _port = (_pin / 8) & 1;
-	uint8_t _p = _pin % 8;
+    uint8_t _port = (_pin / 8) & 1;
+    uint8_t _p = _pin % 8;
 
-	switch (_mode)
-	{
-		case INPUT:
-			mcpRegsInt[MCP23017_IODIRA + _port] |= 1 << _p;   //Set it to input
-			mcpRegsInt[MCP23017_GPPUA + _port] &= ~(1 << _p); //Disable pullup on that pin
-			updateRegister(MCP23017_ADDR, MCP23017_IODIRA + _port, mcpRegsInt[MCP23017_IODIRA + _port]);
-			updateRegister(MCP23017_ADDR, MCP23017_GPPUA + _port, mcpRegsInt[MCP23017_GPPUA + _port]);
-			break;
+    switch (_mode)
+    {
+    case INPUT:
+        mcpRegsInt[MCP23017_IODIRA + _port] |= 1 << _p;   //Set it to input
+        mcpRegsInt[MCP23017_GPPUA + _port] &= ~(1 << _p); //Disable pullup on that pin
+        updateRegister(MCP23017_ADDR, MCP23017_IODIRA + _port, mcpRegsInt[MCP23017_IODIRA + _port]);
+        updateRegister(MCP23017_ADDR, MCP23017_GPPUA + _port, mcpRegsInt[MCP23017_GPPUA + _port]);
+        break;
 
-		case INPUT_PULLUP:
-			mcpRegsInt[MCP23017_IODIRA + _port] |= 1 << _p;   //Set it to input
-			mcpRegsInt[MCP23017_GPPUA + _port] |= 1 << _p;    //Enable pullup on that pin
-			updateRegister(MCP23017_ADDR, MCP23017_IODIRA + _port, mcpRegsInt[MCP23017_IODIRA + _port]);
-			updateRegister(MCP23017_ADDR, MCP23017_GPPUA + _port, mcpRegsInt[MCP23017_GPPUA + _port]);
-			break;
+    case INPUT_PULLUP:
+        mcpRegsInt[MCP23017_IODIRA + _port] |= 1 << _p; //Set it to input
+        mcpRegsInt[MCP23017_GPPUA + _port] |= 1 << _p;  //Enable pullup on that pin
+        updateRegister(MCP23017_ADDR, MCP23017_IODIRA + _port, mcpRegsInt[MCP23017_IODIRA + _port]);
+        updateRegister(MCP23017_ADDR, MCP23017_GPPUA + _port, mcpRegsInt[MCP23017_GPPUA + _port]);
+        break;
 
-		case OUTPUT:
-			mcpRegsInt[MCP23017_IODIRA + _port] &= ~(1 << _p); //Set it to output
-			mcpRegsInt[MCP23017_GPPUA + _port] &= ~(1 << _p); //Disable pullup on that pin
-			updateRegister(MCP23017_ADDR, MCP23017_IODIRA + _port, mcpRegsInt[MCP23017_IODIRA + _port]);
-			updateRegister(MCP23017_ADDR, MCP23017_GPPUA + _port, mcpRegsInt[MCP23017_GPPUA + _port]);
-			break;
-	}
+    case OUTPUT:
+        mcpRegsInt[MCP23017_IODIRA + _port] &= ~(1 << _p); //Set it to output
+        mcpRegsInt[MCP23017_GPPUA + _port] &= ~(1 << _p);  //Disable pullup on that pin
+        updateRegister(MCP23017_ADDR, MCP23017_IODIRA + _port, mcpRegsInt[MCP23017_IODIRA + _port]);
+        updateRegister(MCP23017_ADDR, MCP23017_GPPUA + _port, mcpRegsInt[MCP23017_GPPUA + _port]);
+        break;
+    }
 }
 
 void Inkplate::digitalWriteMCP(uint8_t _pin, uint8_t _state)
 {
-	uint8_t _port = (_pin / 8) & 1;
-	uint8_t _p = _pin % 8;
+    uint8_t _port = (_pin / 8) & 1;
+    uint8_t _p = _pin % 8;
 
-	if (mcpRegsInt[MCP23017_IODIRA + _port] & (1 << _p)) return; //Check if the pin is set as an output
-	_state ? (mcpRegsInt[MCP23017_GPIOA + _port] |= (1 << _p)) : (mcpRegsInt[MCP23017_GPIOA + _port] &= ~(1 << _p));
-	updateRegister(MCP23017_ADDR, MCP23017_GPIOA + _port, mcpRegsInt[MCP23017_GPIOA + _port]);
+    if (mcpRegsInt[MCP23017_IODIRA + _port] & (1 << _p))
+        return; //Check if the pin is set as an output
+    _state ? (mcpRegsInt[MCP23017_GPIOA + _port] |= (1 << _p)) : (mcpRegsInt[MCP23017_GPIOA + _port] &= ~(1 << _p));
+    updateRegister(MCP23017_ADDR, MCP23017_GPIOA + _port, mcpRegsInt[MCP23017_GPIOA + _port]);
 }
 
 uint8_t Inkplate::digitalReadMCP(uint8_t _pin)
 {
-	uint8_t _port = (_pin / 8) & 1;
-	uint8_t _p = _pin % 8;
-	readMCPRegister(MCP23017_ADDR, MCP23017_GPIOA + _port, mcpRegsInt);
-	return (mcpRegsInt[MCP23017_GPIOA + _port] & (1 << _p)) ? HIGH : LOW;
+    uint8_t _port = (_pin / 8) & 1;
+    uint8_t _p = _pin % 8;
+    readMCPRegister(MCP23017_ADDR, MCP23017_GPIOA + _port, mcpRegsInt);
+    return (mcpRegsInt[MCP23017_GPIOA + _port] & (1 << _p)) ? HIGH : LOW;
 }
 
 void Inkplate::setIntOutput(uint8_t intPort, uint8_t mirroring, uint8_t openDrain, uint8_t polarity)
 {
-	intPort &= 1;
-	mirroring &= 1;
-	openDrain &= 1;
-	polarity &= 1;
-	mcpRegsInt[MCP23017_IOCONA + intPort] = (mcpRegsInt[MCP23017_IOCONA + intPort] & ~(1 << 6)) | (1 << mirroring);
-	mcpRegsInt[MCP23017_IOCONA + intPort] = (mcpRegsInt[MCP23017_IOCONA + intPort] & ~(1 << 6)) | (1 << openDrain);
-	mcpRegsInt[MCP23017_IOCONA + intPort] = (mcpRegsInt[MCP23017_IOCONA + intPort] & ~(1 << 6)) | (1 << polarity);
-	updateRegister(MCP23017_ADDR, MCP23017_IOCONA + intPort, mcpRegsInt[MCP23017_IOCONA + intPort]);
+    intPort &= 1;
+    mirroring &= 1;
+    openDrain &= 1;
+    polarity &= 1;
+    mcpRegsInt[MCP23017_IOCONA + intPort] = (mcpRegsInt[MCP23017_IOCONA + intPort] & ~(1 << 6)) | (1 << mirroring);
+    mcpRegsInt[MCP23017_IOCONA + intPort] = (mcpRegsInt[MCP23017_IOCONA + intPort] & ~(1 << 6)) | (1 << openDrain);
+    mcpRegsInt[MCP23017_IOCONA + intPort] = (mcpRegsInt[MCP23017_IOCONA + intPort] & ~(1 << 6)) | (1 << polarity);
+    updateRegister(MCP23017_ADDR, MCP23017_IOCONA + intPort, mcpRegsInt[MCP23017_IOCONA + intPort]);
 }
 
 void Inkplate::setIntPin(uint8_t _pin, uint8_t _mode)
 {
-	uint8_t _port = (_pin / 8) & 1;
-	uint8_t _p = _pin % 8;
+    uint8_t _port = (_pin / 8) & 1;
+    uint8_t _p = _pin % 8;
 
-	switch (_mode)
-	{
-		case CHANGE:
-			mcpRegsInt[MCP23017_INTCONA + _port] &= ~(1 << _p);
-			break;
+    switch (_mode)
+    {
+    case CHANGE:
+        mcpRegsInt[MCP23017_INTCONA + _port] &= ~(1 << _p);
+        break;
 
-		case FALLING:
-			mcpRegsInt[MCP23017_INTCONA + _port] |= (1 << _p);
-			mcpRegsInt[MCP23017_DEFVALA + _port] |= (1 << _p);
-			break;
+    case FALLING:
+        mcpRegsInt[MCP23017_INTCONA + _port] |= (1 << _p);
+        mcpRegsInt[MCP23017_DEFVALA + _port] |= (1 << _p);
+        break;
 
-		case RISING:
-			mcpRegsInt[MCP23017_INTCONA + _port] |= (1 << _p);
-			mcpRegsInt[MCP23017_DEFVALA + _port] &= ~(1 << _p);
-			break;
-  }
-  mcpRegsInt[MCP23017_GPINTENA + _port] |= (1 << _p);
-  updateRegister(MCP23017_ADDR, MCP23017_GPINTENA, mcpRegsInt, 6);
+    case RISING:
+        mcpRegsInt[MCP23017_INTCONA + _port] |= (1 << _p);
+        mcpRegsInt[MCP23017_DEFVALA + _port] &= ~(1 << _p);
+        break;
+    }
+    mcpRegsInt[MCP23017_GPINTENA + _port] |= (1 << _p);
+    updateRegister(MCP23017_ADDR, MCP23017_GPINTENA, mcpRegsInt, 6);
 }
 
 void Inkplate::removeIntPin(uint8_t _pin)
 {
-	uint8_t _port = (_pin / 8) & 1;
-	uint8_t _p = _pin % 8;
-	mcpRegsInt[MCP23017_GPINTENA + _port] &= ~(1 << _p);
-	updateRegister(MCP23017_ADDR, MCP23017_GPINTENA, mcpRegsInt, 2);
+    uint8_t _port = (_pin / 8) & 1;
+    uint8_t _p = _pin % 8;
+    mcpRegsInt[MCP23017_GPINTENA + _port] &= ~(1 << _p);
+    updateRegister(MCP23017_ADDR, MCP23017_GPINTENA, mcpRegsInt, 2);
 }
 
 uint16_t Inkplate::getINT()
 {
-	readMCPRegisters(MCP23017_ADDR, MCP23017_INTFA, mcpRegsInt, 2);
-	return ((mcpRegsInt[MCP23017_INTFB] << 8) | mcpRegsInt[MCP23017_INTFA]);
+    readMCPRegisters(MCP23017_ADDR, MCP23017_INTFA, mcpRegsInt, 2);
+    return ((mcpRegsInt[MCP23017_INTFB] << 8) | mcpRegsInt[MCP23017_INTFA]);
 }
 
 uint16_t Inkplate::getINTstate()
 {
-	readMCPRegisters(MCP23017_ADDR, MCP23017_INTCAPA, mcpRegsInt, 2);
-	return ((mcpRegsInt[MCP23017_INTCAPB] << 8) | mcpRegsInt[MCP23017_INTCAPA]);
+    readMCPRegisters(MCP23017_ADDR, MCP23017_INTCAPA, mcpRegsInt, 2);
+    return ((mcpRegsInt[MCP23017_INTCAPB] << 8) | mcpRegsInt[MCP23017_INTCAPA]);
 }
 
 void Inkplate::setPorts(uint16_t _d)
 {
-	mcpRegsInt[MCP23017_GPIOA] = _d & 0xff;
-	mcpRegsInt[MCP23017_GPIOB] = (_d >> 8) & 0xff;
-	updateRegister(MCP23017_ADDR, MCP23017_GPIOA, mcpRegsInt, 2);
+    mcpRegsInt[MCP23017_GPIOA] = _d & 0xff;
+    mcpRegsInt[MCP23017_GPIOB] = (_d >> 8) & 0xff;
+    updateRegister(MCP23017_ADDR, MCP23017_GPIOA, mcpRegsInt, 2);
 }
 
 uint16_t Inkplate::getPorts()
 {
-	readMCPRegisters(MCP23017_ADDR, MCP23017_GPIOA, mcpRegsInt, 2);
-	return ((mcpRegsInt[MCP23017_GPIOB] << 8) | (mcpRegsInt[MCP23017_GPIOA]));
+    readMCPRegisters(MCP23017_ADDR, MCP23017_GPIOA, mcpRegsInt, 2);
+    return ((mcpRegsInt[MCP23017_GPIOB] << 8) | (mcpRegsInt[MCP23017_GPIOA]));
 }
 
 //--------------------------PRIVATE FUNCTIONS--------------------------------------------
@@ -1125,32 +1128,33 @@ void Inkplate::display1b()
     }
     delayMicroseconds(230);
 
-	vscan_start();
+    vscan_start();
     for (int i = 0; i < 600; i++)
-	{
-		dram = *(D_memory_new + _pos);
-		data = 0b00000000;;
-		_send = ((data & B00000011) << 4) | (((data & B00001100) >> 2) << 18) | (((data & B00010000) >> 4) << 23) | (((data & B11100000) >> 5) << 25);
-		hscan_start(_send);
-		data = 0b00000000;
-		GPIO.out_w1ts = (_send) | CL;
-		GPIO.out_w1tc = DATA | CL;
-		for (int j = 0; j < 99; j++)
-		{
-			GPIO.out_w1ts = (_send) | CL;
-			GPIO.out_w1tc = DATA | CL;
-			GPIO.out_w1ts = (_send) | CL;
-			GPIO.out_w1tc = DATA | CL;
-		}
-		GPIO.out_w1ts = (_send) | CL;
-		GPIO.out_w1tc = DATA | CL;
-		vscan_end();
-	}
-	delayMicroseconds(230);  
-		
-	vscan_start();
-	einkOff();
-	_blockPartial = 0;
+    {
+        dram = *(D_memory_new + _pos);
+        data = 0b00000000;
+        ;
+        _send = ((data & B00000011) << 4) | (((data & B00001100) >> 2) << 18) | (((data & B00010000) >> 4) << 23) | (((data & B11100000) >> 5) << 25);
+        hscan_start(_send);
+        data = 0b00000000;
+        GPIO.out_w1ts = (_send) | CL;
+        GPIO.out_w1tc = DATA | CL;
+        for (int j = 0; j < 99; j++)
+        {
+            GPIO.out_w1ts = (_send) | CL;
+            GPIO.out_w1tc = DATA | CL;
+            GPIO.out_w1ts = (_send) | CL;
+            GPIO.out_w1tc = DATA | CL;
+        }
+        GPIO.out_w1ts = (_send) | CL;
+        GPIO.out_w1tc = DATA | CL;
+        vscan_end();
+    }
+    delayMicroseconds(230);
+
+    vscan_start();
+    einkOff();
+    _blockPartial = 0;
 }
 
 //Display content from RAM to display (3 bit per pixel,. 8 level of grayscale, STILL IN PROGRESSS, we need correct wavefrom to get good picture, use it only for pictures not for GFX).
@@ -1972,78 +1976,79 @@ void Inkplate::precalculateGamma(uint8_t *c, float gamma)
 }
 
 //----------------------------MCP23017 Low Level Functions----------------------------
-bool Inkplate::mcpBegin(uint8_t _addr, uint8_t* _r)
+bool Inkplate::mcpBegin(uint8_t _addr, uint8_t *_r)
 {
-  Wire.beginTransmission(_addr);
-  int error = Wire.endTransmission();
-  if (error) return false;
-  readMCPRegisters(_addr, _r);
-  _r[0] = 0xff;
-  _r[1] = 0xff;
-  updateAllRegisters(_addr, _r);
-  return true;
+    Wire.beginTransmission(_addr);
+    int error = Wire.endTransmission();
+    if (error)
+        return false;
+    readMCPRegisters(_addr, _r);
+    _r[0] = 0xff;
+    _r[1] = 0xff;
+    updateAllRegisters(_addr, _r);
+    return true;
 }
 
 void Inkplate::readMCPRegisters(uint8_t _addr, uint8_t *k)
 {
-	Wire.beginTransmission(_addr);
-	Wire.write(0x00);
-	Wire.endTransmission();
-	Wire.requestFrom(_addr, (uint8_t)22);
-	for (int i = 0; i < 22; i++)
-	{
-		k[i] = Wire.read();
-	}
+    Wire.beginTransmission(_addr);
+    Wire.write(0x00);
+    Wire.endTransmission();
+    Wire.requestFrom(_addr, (uint8_t)22);
+    for (int i = 0; i < 22; i++)
+    {
+        k[i] = Wire.read();
+    }
 }
 
 void Inkplate::readMCPRegisters(uint8_t _addr, uint8_t _regName, uint8_t *k, uint8_t _n)
 {
-	Wire.beginTransmission(_addr);
-	Wire.write(_regName);
-	Wire.endTransmission();
+    Wire.beginTransmission(_addr);
+    Wire.write(_regName);
+    Wire.endTransmission();
 
-	Wire.requestFrom(_addr, _n);
-	for (int i = 0; i < _n; i++)
-	{
-		k[_regName + i] = Wire.read();
-	}
+    Wire.requestFrom(_addr, _n);
+    for (int i = 0; i < _n; i++)
+    {
+        k[_regName + i] = Wire.read();
+    }
 }
 
 void Inkplate::readMCPRegister(uint8_t _addr, uint8_t _regName, uint8_t *k)
 {
-	Wire.beginTransmission(_addr);
-	Wire.write(_regName);
-	Wire.endTransmission();
-	Wire.requestFrom(_addr, (uint8_t)1);
-	k[_regName] = Wire.read();
+    Wire.beginTransmission(_addr);
+    Wire.write(_regName);
+    Wire.endTransmission();
+    Wire.requestFrom(_addr, (uint8_t)1);
+    k[_regName] = Wire.read();
 }
 
 void Inkplate::updateAllRegisters(uint8_t _addr, uint8_t *k)
 {
-	Wire.beginTransmission(_addr);
-	Wire.write(0x00);
-	for (int i = 0; i < 22; i++)
-	{
-		Wire.write(k[i]);
-	}
-	Wire.endTransmission();
+    Wire.beginTransmission(_addr);
+    Wire.write(0x00);
+    for (int i = 0; i < 22; i++)
+    {
+        Wire.write(k[i]);
+    }
+    Wire.endTransmission();
 }
 
 void Inkplate::updateRegister(uint8_t _addr, uint8_t _regName, uint8_t _d)
 {
-	Wire.beginTransmission(_addr);
-	Wire.write(_regName);
-	Wire.write(_d);
-	Wire.endTransmission();
+    Wire.beginTransmission(_addr);
+    Wire.write(_regName);
+    Wire.write(_d);
+    Wire.endTransmission();
 }
 
 void Inkplate::updateRegister(uint8_t _addr, uint8_t _regName, uint8_t *k, uint8_t _n)
 {
-	Wire.beginTransmission(_addr);
-	Wire.write(_regName);
-	for (int i = 0; i < _n; i++)
-	{
-		Wire.write(k[_regName + i]);
-	}
-	Wire.endTransmission();
+    Wire.beginTransmission(_addr);
+    Wire.write(_regName);
+    for (int i = 0; i < _n; i++)
+    {
+        Wire.write(k[_regName + i]);
+    }
+    Wire.endTransmission();
 }
--- examples/2. Advanced Inkplate Features/4-Inkplate_Touchpads/4-Inkplate_Touchpads.ino
+++ examples/2. Advanced Inkplate Features/4-Inkplate_Touchpads/4-Inkplate_Touchpads.ino
@@ -22,53 +22,62 @@
    15 July 2020 by e-radionica.com
 */
 
-#include "Inkplate.h"               //Include Inkplate library to the sketch
-Inkplate display(INKPLATE_1BIT);    //Create an object on Inkplate library and also set library into 1 Bit mode (Monochrome)
+#include "Inkplate.h"            //Include Inkplate library to the sketch
+Inkplate display(INKPLATE_1BIT); //Create an object on Inkplate library and also set library into 1 Bit mode (Monochrome)
 
-int number = 0;                     //Variable that stores our number
-int n = 0;                          //Variable that keeps track on how many times display is partially updated
-void setup() {
-  display.begin();                  //Init Inkplate library (you should call this function ONLY ONCE)
-  display.clearDisplay();           //Clear frame buffer of display
-  display.display();                //Put clear image on display
-  display.setTextSize(5);               //Set text scaling to five (text will be five times bigger)
-  display.setTextColor(BLACK, WHITE);   //Set text color to black and background color to white
-  displayNumber();                      //Call our function to display nubmer on screen
+int number = 0; //Variable that stores our number
+int n = 0;      //Variable that keeps track on how many times display is partially updated
+void setup()
+{
+  display.begin();                    //Init Inkplate library (you should call this function ONLY ONCE)
+  display.clearDisplay();             //Clear frame buffer of display
+  display.display();                  //Put clear image on display
+  display.setTextSize(5);             //Set text scaling to five (text will be five times bigger)
+  display.setTextColor(BLACK, WHITE); //Set text color to black and background color to white
+  displayNumber();                    //Call our function to display nubmer on screen
 }
 
-void loop() {
-  if (display.readTouchpad(PAD1)) {     //Check if first pad has been touched. If it is, decrement the number and refresh the screen.
+void loop()
+{
+  if (display.readTouchpad(PAD1))
+  { //Check if first pad has been touched. If it is, decrement the number and refresh the screen.
     number--;
     displayNumber();
   }
 
-  if (display.readTouchpad(PAD2)) {     //If you touched second touchpad, set number to zero and refresh screen by calling our displayNumber() function
+  if (display.readTouchpad(PAD2))
+  { //If you touched second touchpad, set number to zero and refresh screen by calling our displayNumber() function
     number = 0;
     displayNumber();
   }
 
-  if (display.readTouchpad(PAD3)) {     //If you touched third touchpad, incerement the number and refresh the screen.
+  if (display.readTouchpad(PAD3))
+  { //If you touched third touchpad, incerement the number and refresh the screen.
     number++;
     displayNumber();
   }
-  delay(100);                           //Wait a little bit between readings.
+  delay(100); //Wait a little bit between readings.
 }
 
 //Function that will write you number to screen
-void displayNumber() {
-  display.clearDisplay();         //First, lets delete everything from frame buffer
-  display.setCursor(385, 280);    //Set print cursor at X=385, Y=280 (roughly in the middle of the screen)
-  display.print(number, DEC);     //Print the number
-  display.setCursor(255, 560);    //Set new print position (right above first touchpad)
-  display.print('-');             //Print minus sign
-  display.setCursor(385, 560);    //Set new print position (right above second touchpad) 
-  display.print('0');             //Print zero
-  display.setCursor(520, 560);    //Set new print position (right above third touchpad)
-  display.print('+');             //Print plus sign
-  if (n > 20) {                   //Chech if screen has been partially refreshed more than 20 times. If it is, do a full refresh. If is not, do a partial refresh
+void displayNumber()
+{
+  display.clearDisplay();      //First, lets delete everything from frame buffer
+  display.setCursor(385, 280); //Set print cursor at X=385, Y=280 (roughly in the middle of the screen)
+  display.print(number, DEC);  //Print the number
+  display.setCursor(255, 560); //Set new print position (right above first touchpad)
+  display.print('-');          //Print minus sign
+  display.setCursor(385, 560); //Set new print position (right above second touchpad)
+  display.print('0');          //Print zero
+  display.setCursor(520, 560); //Set new print position (right above third touchpad)
+  display.print('+');          //Print plus sign
+  if (n > 20)
+  { //Chech if screen has been partially refreshed more than 20 times. If it is, do a full refresh. If is not, do a partial refresh
     display.display();
     n = 0;
-  } else {
+  }
+  else
+  {
     display.partialUpdate();
     n++;
   }
