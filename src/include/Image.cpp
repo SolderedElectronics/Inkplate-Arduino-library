9fa259bd0f2d4ed5dc8faa00d333441e100eb3a9
TAVU - Robert <robertperic@outlook.com>
2021-04-01 14:08:26 +0200

    Comments for image file

--- src/include/Image.cpp
+++ src/include/Image.cpp
@@ -1,18 +1,19 @@
-/*
-Image.cpp
-Inkplate Arduino library
-David Zovko, Borna Biro, Denis Vajak, Zvonimir Haramustek @ e-radionica.com
-February 12, 2021
-https://github.com/e-radionicacom/Inkplate-Arduino-library
-
-For support, please reach over forums: forum.e-radionica.com/en
-For more info about the product, please check: www.inkplate.io
-
-This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
-Please review the LICENSE file included with this example.
-If you have any questions about licensing, please contact techsupport@e-radionica.com
-Distributed as-is; no warranty is given.
-*/
+/**
+ **************************************************
+ * @file        Image.cpp
+ * @brief       Basic functionalities to work with images
+ * 
+ *              https://github.com/e-radionicacom/Inkplate-Arduino-library
+ *              For support, please reach over forums: forum.e-radionica.com/en
+ *              For more info about the product, please check: www.inkplate.io
+ *
+ *              This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
+ *              Please review the LICENSE file included with this example.
+ *              If you have any questions about licensing, please contact techsupport@e-radionica.com
+ *              Distributed as-is; no warranty is given.
+ * 
+ * @authors     e-radionica.com
+ ***************************************************/
 
 #include "Image.h"
 
@@ -23,17 +24,52 @@ Distributed as-is; no warranty is given.
 Image *_imagePtrJpeg = nullptr;
 Image *_imagePtrPng = nullptr;
 
+/**
+ * @brief       Image sets pointers to selected image
+ */
 Image::Image(int16_t w, int16_t h) : Adafruit_GFX(w, h)
 {
     _imagePtrJpeg = this;
     _imagePtrPng = this;
 }
 
+/**
+ * @brief       drawImage function draws image from string path
+ * 
+ * @param       String path
+ *              path to image file
+ * @param       int x
+ *              x position for upper left image corner
+ * @param       int y
+ *              y position for upper left image corner
+ * @param       bool dither
+ *              1 if using dither, 0 if not
+ * @param       bool invert
+ *              1 if inverting, 0 if not
+ *
+ * @return      1 if succesfuly drawn, 0 if not             
+ */
 bool Image::drawImage(const String path, int x, int y, bool dither, bool invert)
 {
     return drawImage(path.c_str(), x, y, dither, invert);
 };
 
+/**
+ * @brief       drawImage function draws image from char path
+ * 
+ * @param       char *path
+ *              char pointer path to image file
+ * @param       int x
+ *              x position for upper left image corner
+ * @param       int y
+ *              y position for upper left image corner
+ * @param       bool dither
+ *              1 if using dither, 0 if not
+ * @param       bool invert
+ *              1 if inverting, 0 if not
+ *
+ * @return      1 if succesfuly drawn, 0 if not             
+ */
 bool Image::drawImage(const char *path, int x, int y, bool dither, bool invert)
 {
     if (strncmp(path, "http://", 7) == 0 || strncmp(path, "https://", 8) == 0)
@@ -57,6 +93,26 @@ bool Image::drawImage(const char *path, int x, int y, bool dither, bool invert)
     return 0;
 };
 
+/**
+ * @brief       drawImage function draws image from buffer
+ * 
+ * @param       uint8_t *buf
+ *              char pointer path to image file
+ * @param       int x
+ *              x position for upper left image corner
+ * @param       int y
+ *              y position for upper left image corner
+ * @param       bool w
+ *              image width
+ * @param       bool h
+ *              image height
+ * @param       bool c
+ *              image color
+ * @param       bool bg
+ *              16 bit color to draw background with
+ * 
+ * @return      1 if succesfuly drawn, 0 if not             
+ */
 bool Image::drawImage(const uint8_t *buf, int x, int y, int16_t w, int16_t h, uint8_t c, uint8_t bg)
 {
 #ifdef ARDUINO_INKPLATECOLOR
@@ -72,12 +128,48 @@ bool Image::drawImage(const uint8_t *buf, int x, int y, int16_t w, int16_t h, ui
 #endif
 }
 
+/**
+ * @brief       drawImage function draws image from string path
+ * 
+ * @param       String path
+ *              path to image file
+ * @param       Format &format
+ *              Image format (bmp, jpg, png)
+ * @param       int x
+ *              x position for upper left image corner
+ * @param       int y
+ *              y position for upper left image corner
+ * @param       bool dither
+ *              1 if using dither, 0 if not
+ * @param       bool invert
+ *              1 if inverting, 0 if not
+ *
+ * @return      1 if succesfuly drawn, 0 if not             
+ */
 bool Image::drawImage(const String path, const Format &format, const int x, const int y, const bool dither,
                       const bool invert)
 {
     return drawImage(path.c_str(), format, x, y, dither, invert);
 };
 
+/**
+ * @brief       drawImage function draws image from string path
+ * 
+ * @param       char *path
+ *              path to image file
+ * @param       Format &format
+ *              Image format (bmp, jpg, png)
+ * @param       int x
+ *              x position for upper left image corner
+ * @param       int y
+ *              y position for upper left image corner
+ * @param       bool dither
+ *              1 if using dither, 0 if not
+ * @param       bool invert
+ *              1 if inverting, 0 if not
+ *
+ * @return      1 if succesfuly drawn, 0 if not             
+ */
 bool Image::drawImage(const char *path, const Format &format, const int x, const int y, const bool dither,
                       const bool invert)
 {
@@ -102,6 +194,22 @@ bool Image::drawImage(const char *path, const Format &format, const int x, const
     return 0;
 }
 
+/**
+ * @brief       drawImage function draws image from string path
+ * 
+ * @param       char *path
+ *              path to image file
+ * @param       Format &format
+ *              Image format (bmp, jpg, png)
+ * @param       Position position
+ *              Image position (center, topLeft, bottomLeft, topRight, bottomRight, _npos)
+ * @param       bool dither
+ *              1 if using dither, 0 if not
+ * @param       bool invert
+ *              1 if inverting, 0 if not
+ *
+ * @return      1 if succesfuly drawn, 0 if not             
+ */
 bool Image::drawImage(const char *path, const Format &format, const Position &position, const bool dither,
                       const bool invert)
 {
@@ -128,7 +236,18 @@ bool Image::drawImage(const char *path, const Format &format, const Position &po
     return false;
 }
 
-
+/**
+ * @brief       drawBitmap3Bit function draws bitmap 3 bit image
+ * 
+ * @param       int16_t _x
+ *              x position for image top left cornere
+ * @param       int16_t _y
+ *              x position for image top left cornere
+ * @param       int16_t _w
+ *              Image width
+ * @param       int16_t _h
+ *              Image height
+ */
 void Image::drawBitmap3Bit(int16_t _x, int16_t _y, const unsigned char *_p, int16_t _w, int16_t _h)
 {
 #ifndef ARDUINO_INKPLATECOLOR
