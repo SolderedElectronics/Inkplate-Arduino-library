87340bedc1b1ebe257f2c5e178aa3f52dbb516e1
TAVU - Robert <robertperic@outlook.com>
2021-04-01 13:15:58 +0200

    Documentation for backlight

--- src/include/Backlight.cpp
+++ src/include/Backlight.cpp
@@ -1,24 +1,33 @@
-/*
-Backlight.cpp
-Inkplate Arduino library
-David Zovko, Borna Biro, Denis Vajak, Zvonimir Haramustek @ e-radionica.com
-February 12, 2021
-https://github.com/e-radionicacom/Inkplate-Arduino-library
-
-For support, please reach over forums: forum.e-radionica.com/en
-For more info about the product, please check: www.inkplate.io
-
-This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
-Please review the LICENSE file included with this example.
-If you have any questions about licensing, please contact techsupport@e-radionica.com
-Distributed as-is; no warranty is given.
-*/
+/**
+ **************************************************
+ * @file        Backlight.cpp
+ * @brief       Basic funtions for controling inkplate backlight
+ * 
+ *              https://github.com/e-radionicacom/Inkplate-Arduino-library
+ *              For support, please reach over forums: forum.e-radionica.com/en
+ *              For more info about the product, please check: www.inkplate.io
+ *
+ *              This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
+ *              Please review the LICENSE file included with this example.
+ *              If you have any questions about licensing, please contact techsupport@e-radionica.com
+ *              Distributed as-is; no warranty is given.
+ * 
+ * @authors     e-radionica.com
+ ***************************************************/
 
 #include "Backlight.h"
 #include "defines.h"
 
 #ifdef ARDUINO_INKPLATE6PLUS
 
+/**
+ * @brief       setBacklight function sets backlight intensity for inkplate
+ * 
+ * @param       uint8_t _v
+ *              value to set backlight to
+ * 
+ * @note        can only be used in inkplate6plus, others don't suport backlight
+ */
 void Backlight::setBacklight(uint8_t _v)
 {
     Wire.beginTransmission(0x5C >> 1);
@@ -27,6 +36,12 @@ void Backlight::setBacklight(uint8_t _v)
     Wire.endTransmission();
 }
 
+/**
+ * @brief       backlight function turns backlight on/off
+ * 
+ * @param       bool _e
+ *              enable value, 1 turns on, 0 off
+ */
 void Backlight::backlight(bool _e)
 {
     if (_e)
--- src/include/Backlight.h
+++ src/include/Backlight.h
@@ -1,19 +1,19 @@
-/*
-Backlight.h
-Inkplate Arduino library
-David Zovko, Borna Biro, Denis Vajak, Zvonimir Haramustek @ e-radionica.com
-February 12, 2021
-https://github.com/e-radionicacom/Inkplate-Arduino-library
-
-For support, please reach over forums: forum.e-radionica.com/en
-For more info about the product, please check: www.inkplate.io
-
-This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
-Please review the LICENSE file included with this example.
-If you have any questions about licensing, please contact techsupport@e-radionica.com
-Distributed as-is; no warranty is given.
-*/
-
+/**
+ **************************************************
+ * @file        Backlight.h
+ * @brief       Basic funtions for controling inkplate backlight
+ * 
+ *              https://github.com/e-radionicacom/Inkplate-Arduino-library
+ *              For support, please reach over forums: forum.e-radionica.com/en
+ *              For more info about the product, please check: www.inkplate.io
+ *
+ *              This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
+ *              Please review the LICENSE file included with this example.
+ *              If you have any questions about licensing, please contact techsupport@e-radionica.com
+ *              Distributed as-is; no warranty is given.
+ * 
+ * @authors     e-radionica.com
+ ***************************************************/
 #ifndef __BACKLIGHT_H__
 #define __BACKLIGHT_H__
 
@@ -24,6 +24,9 @@ Distributed as-is; no warranty is given.
 #include "Wire.h"
 #include "defines.h"
 
+/**
+ * @brief       Backlight class designed to work with inkplates that support backlight (Inkplate 6 plus)
+ */
 class Backlight : virtual public Mcp
 {
   public:
