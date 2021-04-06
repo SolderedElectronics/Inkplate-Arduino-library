d5b041f2976caa2a495529372c0715b3b9a63366
TAVU - Robert <robertperic@outlook.com>
2021-04-06 11:30:36 +0200

    Documentation for system.cpp

--- src/include/System.cpp
+++ src/include/System.cpp
@@ -1,34 +1,51 @@
-/*
-System.cpp
-Inkplate Arduino library
-David Zovko, Borna Biro, Denis Vajak, Zvonimir Haramustek @ e-radionica.com
-February 12, 2021
-https://github.com/e-radionicacom/Inkplate-Arduino-library
-
-For support, please reach over forums: forum.e-radionica.com/en
-For more info about the product, please check: www.inkplate.io
-
-This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
-Please review the LICENSE file included with this example.
-If you have any questions about licensing, please contact techsupport@e-radionica.com
-Distributed as-is; no warranty is given.
-*/
+/**
+ **************************************************
+ * @file        System.cpp
+ * @brief       Functionality for panel hardware interaction
+ * 
+ *              https://github.com/e-radionicacom/Inkplate-Arduino-library
+ *              For support, please reach over forums: forum.e-radionica.com/en
+ *              For more info about the product, please check: www.inkplate.io
+ *
+ *              This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
+ *              Please review the LICENSE file included with this example.
+ *              If you have any questions about licensing, please contact techsupport@e-radionica.com
+ *              Distributed as-is; no warranty is given.
+ * 
+ * @authors     @ e-radionica.com
+ ***************************************************/
 
 #include "System.h"
 
 SPIClass spi2(HSPI);
 SdFat sd(&spi2);
 
+/**
+ * @brief       setPanelState sets panel state (on/off)
+ * 
+ * @param       uint8_t s
+ *              panel state ON or OFF (1 or 0)
+ */
 void System::setPanelState(uint8_t s)
 {
     _panelOn = s;
 }
 
+/**
+ * @brief       getPanelState gets panel state (ON/OFF)
+ * 
+ * @return      returns value that represents ON/OFF state of the panel
+ */
 uint8_t System::getPanelState()
 {
     return _panelOn;
 }
 
+/**
+ * @brief       readTemperature reads panel temperature
+ * 
+ * @return      returns  temperature in range from -10 to 85 degree C with accuracy of +-1 in range from 0 to 50
+ */
 int8_t System::readTemperature()
 {
     int8_t temp;
@@ -59,11 +76,24 @@ int8_t System::readTemperature()
     return temp;
 }
 
+/**
+ * @brief       readTouchpad reads touchpad pin to check if triggered
+ * 
+ * @param       uint8_t _pad
+ *              touchpad pin to check
+ * 
+ * @return      value to indicate if touchpad is triggered (TRUE or FALSE)
+ */
 uint8_t System::readTouchpad(uint8_t _pad)
 {
     return digitalReadInternal(MCP23017_INT_ADDR, mcpRegsInt, _pad);
 }
 
+/**
+ * @brief       readBattery reads voltage of the battery
+ * 
+ * @return      returns battery voltage value
+ */
 double System::readBattery()
 {
 #ifdef ARDUINO_ESP32_DEV
@@ -84,6 +114,11 @@ double System::readBattery()
     return (double(adc) / 4095 * 1.1 * 3.548133892 * 2);
 }
 
+/**
+ * @brief       sdCardInit initializes sd card trough SPI
+ * 
+ * @return      0 if failed to initialise, 1 if successful
+ */
 int16_t System::sdCardInit()
 {
     spi2.begin(14, 12, 13, 15);
@@ -91,21 +126,42 @@ int16_t System::sdCardInit()
     return getSdCardOk();
 }
 
+/**
+ * @brief       getSdFat gets sd card object
+ * 
+ * @return      sd card class object
+ */
 SdFat System::getSdFat()
 {
     return sd;
 }
 
+/**
+ * @brief       getSPI gets SPI class object
+ * 
+ * @return      SPI class object
+ */
 SPIClass System::getSPI()
 {
     return spi2;
 }
 
+/**
+ * @brief       setSdCardOk sets sd card OK status
+ * 
+ * @param       int16_t s
+ *              sd card OK status, can be 1 or 0
+ */
 void System::setSdCardOk(int16_t s)
 {
     _sdCardOk = s;
 }
 
+/**
+ * @brief       setSdCardOk gets sd card OK status
+ * 
+ * @return      sd card OK status, can be 1 or 0
+ */
 int16_t System::getSdCardOk()
 {
     return _sdCardOk;
--- src/include/System.h
+++ src/include/System.h
@@ -1,18 +1,19 @@
-/*
-System.h
-Inkplate Arduino library
-David Zovko, Borna Biro, Denis Vajak, Zvonimir Haramustek @ e-radionica.com
-February 12, 2021
-https://github.com/e-radionicacom/Inkplate-Arduino-library
-
-For support, please reach over forums: forum.e-radionica.com/en
-For more info about the product, please check: www.inkplate.io
-
-This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
-Please review the LICENSE file included with this example.
-If you have any questions about licensing, please contact techsupport@e-radionica.com
-Distributed as-is; no warranty is given.
-*/
+/**
+ **************************************************
+ * @file        System.h
+ * @brief       Functionality for panel hardware interaction
+ * 
+ *              https://github.com/e-radionicacom/Inkplate-Arduino-library
+ *              For support, please reach over forums: forum.e-radionica.com/en
+ *              For more info about the product, please check: www.inkplate.io
+ *
+ *              This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
+ *              Please review the LICENSE file included with this example.
+ *              If you have any questions about licensing, please contact techsupport@e-radionica.com
+ *              Distributed as-is; no warranty is given.
+ * 
+ * @authors     @ e-radionica.com
+ ***************************************************/
 
 #ifndef __SYSTEM_H__
 #define __SYSTEM_H__
@@ -32,6 +33,9 @@ Distributed as-is; no warranty is given.
 
 #include "defines.h"
 
+/**
+ * @brief       System class for interaction with panel harware
+ */
 class System : public Esp,
                virtual public Mcp,
                virtual public NetworkClient
