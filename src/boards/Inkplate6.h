f103c19e749f84a755a7c04c60c0cf235fc5352c
TAVU - Robert <robertperic@outlook.com>
2021-04-07 08:51:36 +0200

    Documentation for inkplate 5 and 6 files

--- src/boards/Inkplate6.h
+++ src/boards/Inkplate6.h
@@ -1,3 +1,21 @@
+/**
+ **************************************************
+ *
+ * @file        Inkplate6.h
+ * @brief       Basic funtions for controling inkplate 6
+ * 
+ *              https://github.com/e-radionicacom/Inkplate-Arduino-library
+ *              For support, please reach over forums: forum.e-radionica.com/en
+ *              For more info about the product, please check: www.inkplate.io
+ *
+ *              This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
+ *              Please review the LICENSE file included with this example.
+ *              If you have any questions about licensing, please contact techsupport@e-radionica.com
+ *              Distributed as-is; no warranty is given.
+ * 
+ * @authors     @ e-radionica.com
+ ***************************************************/
+
 #ifndef INKPLATE6_H
 #define INKPLATE6_H
 
