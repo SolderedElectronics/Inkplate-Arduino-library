f1261b608cb8d1bdeda21712bc8d3a0d52ebafc2
TAVU - Robert <robertperic@outlook.com>
2021-05-13 11:29:30 +0200

    Added secret light show

--- examples/Inkplate6PLUS/Basic_Inkplate_Functionality/Inkplate_Basic_Backlight/Inkplate_basic_Backlight.ino
+++ examples/Inkplate6PLUS/Basic_Inkplate_Functionality/Inkplate_Basic_Backlight/Inkplate_basic_Backlight.ino
@@ -1,10 +1,10 @@
 /*
-   Inkplate_basic_backlight example for e-radionica.com Inkplate 6PLUS
+   Inkplate_basic_frontlight example for e-radionica.com Inkplate 6PLUS
    For this example you will need a micro USB cable, Inkplate 6PLUS and a device with WiFi and Internet brower (PC,
    Laptop, Smartphone, ...). Select "Inkplate 6PLUS(ESP32)" from Tools -> Board menu. Don't have "Inkplate
    6PLUS(ESP32)" option? Follow our tutorial and add it: https://e-radionica.com/en/blog/add-inkplate-6-to-arduino-ide/
 
-   This example will show you how to use Inkplate 6PLUS backlight.
+   This example will show you how to use Inkplate 6PLUS frontlight.
 
    Want to learn more about Inkplate? Visit www.inkplate.io
    Looking to get support? Write on our forums: http://forum.e-radionica.com/en/
@@ -20,39 +20,60 @@
 
 Inkplate display(INKPLATE_1BIT); // Create an object on Inkplate class
 
-int b = 31; // Variable that holds intensity of the backlight
+int b = 31; // Variable that holds intensity of the frontlight
 
 void setup()
 {
     Serial.begin(115200);    // Set up a serial communication of 115200 baud
     display.begin();         // Init Inkplate library
-    display.backlight(true); // Enable backlight circuit
-    display.setBacklight(b); // Set backlight intensity
+    display.frontlight(true); // Enable frontlight circuit
+    display.setFrontlight(b); // Set frontlight intensity
 }
 
 void loop()
 {
-    if (Serial.available()) // Change backlight value by sending "+" sign into serial monitor to increase backlight or
-                            // "-" sign to decrese backlight
+    if (Serial.available()) // Change frontlight value by sending "+" sign into serial monitor to increase frontlight or
+                            // "-" sign to decrese frontlight
+                            // try to find hidden lightshow ;)
     {
-        bool change = false; // Variable that indicates that backlight value has changed and intessity has to be updated
+        bool change = false;    // Variable that indicates that frontlight value has changed and intessity has to be updated
         char c = Serial.read(); // Read incomming serial data
 
-        if (c == '+' && b < 63) // If is received +, increase backlight
+        if (c == '+' && b < 63) // If is received +, increase frontlight
         {
             b++;
             change = true;
         }
-        if (c == '-' && b > 0) // If is received -, decrease backlight
+        if (c == '-' && b > 0) // If is received -, decrease frontlight
         {
             b--;
             change = true;
         }
 
-        if (change) // If backlight valuse has changed, update the intensity and show current value of backlight
+        if (c == 's')
         {
-            display.setBacklight(b);
-            Serial.print("Backlight:");
+            for (int j = 0; j < 4; ++j)
+            {
+                for (int i = 0; i < 64; ++i)
+                {
+                    display.setFrontlight(i);
+                    delay(30);
+                }
+
+                for (int i = 63; i >= 0; --i)
+                {
+                    display.setFrontlight(i);
+                    delay(30);
+                }
+            }
+
+            change = true;
+        }
+
+        if (change) // If frontlight valuse has changed, update the intensity and show current value of frontlight
+        {
+            display.setFrontlight(b);
+            Serial.print("Frontlight:");
             Serial.print(b, DEC);
             Serial.println("/63");
         }
