259fe7e3c7d187370ff639347688f627048d7345
TAVU - Robert <robertperic@outlook.com>
2021-06-10 16:35:54 +0200

    Added delay variable

--- test/sdCardColorTest/sdCardColorTest.ino
+++ test/sdCardColorTest/sdCardColorTest.ino
@@ -15,6 +15,8 @@ SdFile file;
 
 int invert = 0;
 int dither = 0;
+int delayTime = 2000;
+
 void setup()
 {
     display.begin();
@@ -29,52 +31,52 @@ void setup()
         Serial.println("1 bit mono bmp.");
         display.drawImage("parrots_mono.bmp", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("1 bit indexed bmp.");
         display.drawImage("parrots1_indexed.bmp", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("4 bit indexed bmp.");
         display.drawImage("parrots4_indexed.bmp", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("8 bit indexed bmp.");
         display.drawImage("parrots8_indexed.bmp", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("16 bit 5-6-5 RGB bmp.");
         display.drawImage("parrots16_5-6-5_RGB.bmp", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("16 bit 5-5-5-1 RGB bmp.");
         display.drawImage("parrots16_5-5-5-1_RGB.bmp", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("16 bit 5-5-5-1 RGBA bmp.");
         display.drawImage("parrots16_5-5-5-1_RGBA.bmp", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("24_trueColor bmp.");
         display.drawImage("parrots24_trueColor.bmp", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
-        Serial.println("colors32_RGB bmp.");
+        Serial.println("32_RGB bmp.");
         display.drawImage("parrots32_RGB.bmp", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("32_RGBA bmp.");
         display.drawImage("parrots32_RGBA.bmp", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
     }
     
     if(PNG_TEST)
@@ -82,37 +84,37 @@ void setup()
         Serial.println("mono png.");
         display.drawImage("parrots_mono.png", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("4 bit png.");
         display.drawImage("parrots4.png", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("8 bit png.");
         display.drawImage("parrots8.png", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("24 bit png.");
         display.drawImage("parrots24_RGB.png", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("32 RGBA png.");
         display.drawImage("parrots32_RGBA.png", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("48 RGBA png.");
         display.drawImage("parrots48_RGB.png", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
     
         Serial.println("64 RGBA png.");
         display.drawImage("parrots64_RGBA.png", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
     }
 
     if(JPG_TEST)
@@ -120,18 +122,23 @@ void setup()
         Serial.println("grayscale jpg.");
         display.drawImage("parrots_grayscale.jpg", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
         Serial.println("24 true color jpg.");
         display.drawImage("parrots24_trueColor.jpg", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
 
-        Serial.println("32 trueColor jpg..");
+        Serial.println("32 trueColor jpg.");
         display.drawImage("parrots32_trueColor.jpg", 0, 0, dither, invert);
         display.display();
-        delay(5000);
+        delay(delayTime);
     }
+
+    Serial.println("paintball");
+    display.drawImage("paintball.jpg", 0, 0, dither, invert);
+    display.display();
+    delay(delayTime);
 }
 
 void loop()
