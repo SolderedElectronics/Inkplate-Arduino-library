cc2c2c856dab27413c267c921b36f5ddca4c9d28
TAVU - Robert <robertperic@outlook.com>
2021-06-10 16:36:27 +0200

    Easier handling and changing of filters

--- src/include/ImageDitherColorKernels.h
+++ src/include/ImageDitherColorKernels.h
@@ -21,23 +21,28 @@
 
 // If you want to experiment with different color dither kernels, uncomment the one you want below
 
+
 // Floyd Steinberg
-// const int _coef = 16;
-// const int _kernelX = 1;
-// const unsigned char _kernel[3][4] = {
-//     {0, 0, 7},
-//     {3, 5, 1},
-// };
+const int _coef = 16;
+const int _kernelX = 1;
+const unsigned char _kernel[3][4] = {
+    {0, 0, 7},
+    {3, 5, 1},
+};
 
-// J F Jarvis, C N Judice, and W H Ninke "Minimized Average Error"
-// const int _coef = 48;
-// const int _kernelX = 2;
-// const unsigned char _kernel[3][5] = {
-//     {0, 0, 0, 7, 5},
-//     {3, 5, 7, 5, 3},
-//     {1, 3, 5, 3, 1},
-// };
 
+/*
+//J F Jarvis, C N Judice, and W H Ninke "Minimized Average Error"
+const int _coef = 48;
+const int _kernelX = 2;
+const unsigned char _kernel[3][5] = {
+    {0, 0, 0, 7, 5},
+    {3, 5, 7, 5, 3},
+    {1, 3, 5, 3, 1},
+};
+*/
+
+/*
 // Atkinson
 const int _coef = 8;
 const int _kernelX = 1;
@@ -46,34 +51,40 @@ const unsigned char _kernel[3][4] = {
     {1, 1, 1, 0},
     {0, 1, 0, 0},
 };
+*/
 
+/*
 // Burkes
-// const int _coef = 32;
-// const int _kernelX = 2;
-// const unsigned char _kernel[3][5] = {
-//     {0, 0, 0, 8, 4},
-//     {2, 4, 8, 4, 2},
-//     {0, 0, 0, 0, 0},
-// };
+const int _coef = 32;
+const int _kernelX = 2;
+const unsigned char _kernel[3][5] = {
+    {0, 0, 0, 8, 4},
+    {2, 4, 8, 4, 2},
+    {0, 0, 0, 0, 0},
+};
+*/
 
+/*
 // Stucki
-// const int _coef = 42;
-// const int _kernelX = 2;
-// const unsigned char _kernel[3][5] = {
-//     {0, 0, 0, 8, 4},
-//     {2, 4, 8, 4, 2},
-//     {1, 2, 4, 2, 1},
-// };
+const int _coef = 42;
+const int _kernelX = 2;
+const unsigned char _kernel[3][5] = {
+    {0, 0, 0, 8, 4},
+    {2, 4, 8, 4, 2},
+    {1, 2, 4, 2, 1},
+};
+*/
 
+/*
 // Sierra lite
-// const int _coef = 4;
-// const int _kernelX = 1;
-// const unsigned char _kernel[3][3] = {
-//     {0, 0, 2},
-//     {1, 1, 0},
-//     {0, 0, 0},
-// };
-
+const int _coef = 4;
+const int _kernelX = 1;
+const unsigned char _kernel[3][3] = {
+    {0, 0, 2},
+    {1, 1, 0},
+    {0, 0, 0},
+};
+*/
 const int _kernelWidth = (sizeof _kernel[0] / sizeof _kernel[0][0]);
 const int _kernelHeight = (sizeof _kernel / sizeof _kernel[0]);
 
