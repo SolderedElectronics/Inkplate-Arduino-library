5ca567daae6e93efeefb1a72f72bac2069fc1e7c
TAVU - Robert <robertperic@outlook.com>
2021-08-03 14:42:10 +0200

    Revert "Merge branch 'master' into dev"
    This reverts commit 784d9149f9a3e781c9b8e6b38e74020c4d9eda91, reversing
    changes made to e3811a0ac39173c63623ad6bb32efddfcdb9cf73.

---
+++ examples/Inkplate5/Projects/Daily_Weather_Station_Example/icons.h
@@ -0,0 +1,26 @@
+#ifndef ICONS_H
+#define ICONS_H
+
+#include "binary_Icons/icon_C.h"
+#include "binary_Icons/icon_H.h"
+#include "binary_Icons/icon_Hc.h"
+#include "binary_Icons/icon_Hr.h"
+#include "binary_Icons/icon_Lc.h"
+#include "binary_Icons/icon_Lr.h"
+#include "binary_Icons/icon_S.h"
+#include "binary_Icons/icon_Sl.h"
+#include "binary_Icons/icon_Sn.h"
+#include "binary_Icons/icon_T.h"
+
+#include "binary_Icons/icon_S_C.h"
+#include "binary_Icons/icon_S_H.h"
+#include "binary_Icons/icon_S_Hc.h"
+#include "binary_Icons/icon_S_Hr.h"
+#include "binary_Icons/icon_S_Lc.h"
+#include "binary_Icons/icon_S_Lr.h"
+#include "binary_Icons/icon_S_S.h"
+#include "binary_Icons/icon_S_Sl.h"
+#include "binary_Icons/icon_S_Sn.h"
+#include "binary_Icons/icon_S_T.h"
+
+#endif
