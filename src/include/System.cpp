537f3a283788f48ae64780412d0ca0723a57d5da
nitko12 <nitko12@hotmail.com>
2021-08-19 12:12:53 +0200

    RTC Examples

--- src/include/System.cpp
+++ src/include/System.cpp
@@ -168,3 +168,450 @@ int16_t System::getSdCardOk()
 {
     return _sdCardOk;
 }
+
+#ifdef ARDUINO_INKPLATE6PLUS
+
+
+/**
+ * @brief                   Method to set time
+ *
+ * @param uint8_t rtcHour      Set the rtcHour
+ * @param uint8_t rtcMinute    Set the minutes
+ * @param uint8_t rtcSecond    Set the seconds
+ */
+void System::rtcSetTime(uint8_t rtcHour, uint8_t rtcMinute, uint8_t rtcSecond)
+{
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(RTC_SECOND_ADDR);
+    Wire.write(rtcDecToBcd(rtcSecond));
+    Wire.write(rtcDecToBcd(rtcMinute));
+    Wire.write(rtcDecToBcd(rtcHour));
+    Wire.endTransmission();
+}
+
+/**
+ * @brief                   Method to set the date
+ *
+ * @param uint8_t rtcWeekday   Set the rtcWeekday
+ * @param uint8_t rtcDay       Set the rtcDay
+ * @param uint8_t rtcMonth     Set the rtcMonth
+ * @param uint8_t yr        Set the rtcYear
+ */
+void System::rtcSetDate(uint8_t rtcWeekday, uint8_t rtcDay, uint8_t rtcMonth, uint16_t yr)
+{
+    rtcYear = yr - 1970; // convert to RTC rtcYear format 0-99
+
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(RTC_DAY_ADDR);
+    Wire.write(rtcDecToBcd(rtcDay));
+    Wire.write(rtcDecToBcd(rtcWeekday));
+    Wire.write(rtcDecToBcd(rtcMonth));
+    Wire.write(rtcDecToBcd(rtcYear));
+    Wire.endTransmission();
+}
+
+/**
+ * @brief                   Reads time and date from the RTC
+ */
+void System::rtcReadTime()
+{
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(RTC_SECOND_ADDR); // datasheet 8.4.
+    Wire.endTransmission();
+
+    Wire.requestFrom(I2C_ADDR, 7);
+
+    while (Wire.available())
+    {
+        rtcSecond = rtcBcdToDec(Wire.read() & 0x7F); // ignore bit 7
+        rtcMinute = rtcBcdToDec(Wire.read() & 0x7F);
+        rtcHour = rtcBcdToDec(Wire.read() & 0x3F); // ignore bits 7 & 6
+        rtcDay = rtcBcdToDec(Wire.read() & 0x3F);
+        rtcWeekday = rtcBcdToDec(Wire.read() & 0x07); // ignore bits 7,6,5,4 & 3
+        rtcMonth = rtcBcdToDec(Wire.read() & 0x1F);   // ignore bits 7,6 & 5
+        rtcYear = rtcBcdToDec(Wire.read()) + 1970;
+    }
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current seconds
+ */
+uint8_t System::rtcGetSecond()
+{
+    rtcReadTime();
+    return rtcSecond;
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current minutes
+ */
+uint8_t System::rtcGetMinute()
+{
+    rtcReadTime();
+    return rtcMinute;
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current hours
+ */
+uint8_t System::rtcGetHour()
+{
+    rtcReadTime();
+    return rtcHour;
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current rtcDay
+ */
+uint8_t System::rtcGetDay()
+{
+    rtcReadTime();
+    return rtcDay;
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current rtcWeekday
+ */
+uint8_t System::rtcGetWeekday()
+{
+    rtcReadTime();
+    return rtcWeekday;
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current rtcMonth
+ */
+uint8_t System::rtcGetMonth()
+{
+    rtcReadTime();
+    return rtcMonth;
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current rtcYear
+ */
+uint16_t System::rtcGetYear()
+{
+    rtcReadTime();
+    return rtcYear;
+}
+
+/**
+ * @brief                   Enables the alarm of the RTC
+ */
+void System::rtcEnableAlarm() // datasheet 8.5.6.
+{
+    // check Table 2. Control_2
+    rtcControl2 = RTC_CTRL_2_DEFAULT | RTC_ALARM_AIE; // enable interrupt
+    rtcControl2 &= ~RTC_ALARM_AF;                     // clear alarm flag
+
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(RTC_CTRL_2);
+    Wire.write(rtcControl2);
+    Wire.endTransmission();
+}
+
+/**
+ * @brief                       Sets the alarm to all the params
+ *
+ * @param uint8_t rtcAlarmSecond  Set the alarm seconds
+ * @param uint8_t rtcAlarmMinute  Set the alarm minutes
+ * @param uint8_t rtcAlarmHour    Set the alarm hours
+ * @param uint8_t rtcAlarmDay     Set the alarm rtcDay
+ * @param uint8_t rtcAlarmWeekday Set the alarm rtcWeekday
+ */
+void System::rtcSetAlarm(uint8_t rtcAlarmSecond, uint8_t rtcAlarmMinute, uint8_t rtcAlarmHour, uint8_t rtcAlarmDay,
+                         uint8_t rtcAlarmWeekday)
+{
+    if (rtcAlarmSecond < 99)
+    { // rtcSecond
+        rtcAlarmSecond = constrain(rtcAlarmSecond, 0, 59);
+        rtcAlarmSecond = rtcDecToBcd(rtcAlarmSecond);
+        rtcAlarmSecond &= ~RTC_ALARM;
+    }
+    else
+    {
+        rtcAlarmSecond = 0x0;
+        rtcAlarmSecond |= RTC_ALARM;
+    }
+
+    if (rtcAlarmMinute < 99)
+    { // rtcMinute
+        rtcAlarmMinute = constrain(rtcAlarmMinute, 0, 59);
+        rtcAlarmMinute = rtcDecToBcd(rtcAlarmMinute);
+        rtcAlarmMinute &= ~RTC_ALARM;
+    }
+    else
+    {
+        rtcAlarmMinute = 0x0;
+        rtcAlarmMinute |= RTC_ALARM;
+    }
+
+    if (rtcAlarmHour < 99)
+    { // rtcHour
+        rtcAlarmHour = constrain(rtcAlarmHour, 0, 23);
+        rtcAlarmHour = rtcDecToBcd(rtcAlarmHour);
+        rtcAlarmHour &= ~RTC_ALARM;
+    }
+    else
+    {
+        rtcAlarmHour = 0x0;
+        rtcAlarmHour |= RTC_ALARM;
+    }
+
+    if (rtcAlarmDay < 99)
+    { // rtcDay
+        rtcAlarmDay = constrain(rtcAlarmDay, 1, 31);
+        rtcAlarmDay = rtcDecToBcd(rtcAlarmDay);
+        rtcAlarmDay &= ~RTC_ALARM;
+    }
+    else
+    {
+        rtcAlarmDay = 0x0;
+        rtcAlarmDay |= RTC_ALARM;
+    }
+
+    if (rtcAlarmWeekday < 99)
+    { // rtcWeekday
+        rtcAlarmWeekday = constrain(rtcAlarmWeekday, 0, 6);
+        rtcAlarmWeekday = rtcDecToBcd(rtcAlarmWeekday);
+        rtcAlarmWeekday &= ~RTC_ALARM;
+    }
+    else
+    {
+        rtcAlarmWeekday = 0x0;
+        rtcAlarmWeekday |= RTC_ALARM;
+    }
+
+    rtcEnableAlarm();
+
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(RTC_SECOND_ALARM);
+    Wire.write(rtcAlarmSecond);
+    Wire.write(rtcAlarmMinute);
+    Wire.write(rtcAlarmHour);
+    Wire.write(rtcAlarmDay);
+    Wire.write(rtcAlarmWeekday);
+    Wire.endTransmission();
+}
+
+
+/**
+ * @brief                   Reads the alarm of the RTC
+ */
+void System::rtcReadAlarm()
+{
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(RTC_SECOND_ALARM); // datasheet 8.4.
+    Wire.endTransmission();
+
+    Wire.requestFrom(I2C_ADDR, 5);
+
+    while (Wire.available())
+    {
+        rtcAlarmSecond = Wire.read();   // read RTC_SECOND_ALARM register
+        if (RTC_ALARM & rtcAlarmSecond) // check is AEN = 1 (rtcSecond alarm disabled)
+        {
+            rtcAlarmSecond = 99; // using 99 as code for no alarm
+        }
+        else
+        {                                                              // else if AEN = 0 (rtcSecond alarm enabled)
+            rtcAlarmSecond = rtcBcdToDec(rtcAlarmSecond & ~RTC_ALARM); // remove AEN flag and convert to dec number
+        }
+
+        rtcAlarmMinute = Wire.read(); // rtcMinute
+        if (RTC_ALARM & rtcAlarmMinute)
+        {
+            rtcAlarmMinute = 99;
+        }
+        else
+        {
+            rtcAlarmMinute = rtcBcdToDec(rtcAlarmMinute & ~RTC_ALARM);
+        }
+
+        rtcAlarmHour = Wire.read(); // rtcHour
+        if (RTC_ALARM & rtcAlarmHour)
+        {
+            rtcAlarmHour = 99;
+        }
+        else
+        {
+            rtcAlarmHour = rtcBcdToDec(rtcAlarmHour & 0x3F); // remove bits 7 & 6
+        }
+
+        rtcAlarmDay = Wire.read(); // rtcDay
+        if (RTC_ALARM & rtcAlarmDay)
+        {
+            rtcAlarmDay = 99;
+        }
+        else
+        {
+            rtcAlarmDay = rtcBcdToDec(rtcAlarmDay & 0x3F); // remove bits 7 & 6
+        }
+
+        rtcAlarmWeekday = Wire.read(); // rtcWeekday
+        if (RTC_ALARM & rtcAlarmWeekday)
+        {
+            rtcAlarmWeekday = 99;
+        }
+        else
+        {
+            rtcAlarmWeekday = rtcBcdToDec(rtcAlarmWeekday & 0x07); // remove bits 7,6,5,4 & 3
+        }
+    }
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current alarm seconds
+ */
+uint8_t System::rtcGetAlarmSecond()
+{
+    rtcReadAlarm();
+    return rtcAlarmSecond;
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current alarm minutes
+ */
+uint8_t System::rtcGetAlarmMinute()
+{
+    rtcReadAlarm();
+    return rtcAlarmMinute;
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current alarm hours
+ */
+uint8_t System::rtcGetAlarmHour()
+{
+    rtcReadAlarm();
+    return rtcAlarmHour;
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current alarm rtcDay
+ */
+uint8_t System::rtcGetAlarmDay()
+{
+    rtcReadAlarm();
+    return rtcAlarmDay;
+}
+
+/**
+ * @brief                   Small user method
+ *
+ * @returns uint8_t         Returns the current alarm rtcWeekday
+ */
+uint8_t System::rtcGetAlarmWeekday()
+{
+    rtcReadAlarm();
+    return rtcAlarmWeekday;
+}
+
+/**
+ * @brief                   Sets the timer countdown
+ */
+void System::rtcTimerSet(rtcCountdownSrcClock source_clock, uint8_t value, bool int_enable, bool int_pulse)
+{
+    uint8_t timer_reg[2] = {0};
+
+    // disable the countdown timer
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(RTC_TIMER_MODE);
+    Wire.write(0x18); // default
+    Wire.endTransmission();
+
+    // clear Control_2
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(RTC_CTRL_2);
+    Wire.write(0x00); // default
+    Wire.endTransmission();
+
+    // reconfigure timer
+    timer_reg[1] |= RTC_TIMER_TE; // enable timer
+    if (int_enable)
+        timer_reg[1] |= RTC_TIMER_TIE; // enable interrupr
+    if (int_pulse)
+        timer_reg[1] |= RTC_TIMER_TI_TP; // interrupt mode
+    timer_reg[1] |= source_clock << 3;   // clock source
+    // timer_reg[1] = 0b00011111;
+
+    timer_reg[0] = value;
+
+    // write timer value
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(RTC_TIMER_VAL);
+    Wire.write(timer_reg[0]);
+    Wire.write(timer_reg[1]);
+    Wire.endTransmission();
+}
+
+/**
+ * @brief                   Returns is the timer flag on
+ *
+ * @returns bool            Returns true if the timer flag is on
+ */
+bool System::rtcCheckTimerFlag()
+{
+    uint8_t _crtl_2 = RTC_TIMER_FLAG;
+
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(RTC_CTRL_2);
+    Wire.endTransmission();
+    Wire.requestFrom(I2C_ADDR, 1);
+    _crtl_2 &= Wire.read();
+
+    return _crtl_2;
+}
+
+/**
+ * @brief                   Resets the timer
+ */
+void System::rtcReset() // datasheet 8.2.1.3.
+{
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(RTC_CTRL_1);
+    Wire.write(0x58);
+    Wire.endTransmission();
+}
+
+/**
+ * @brief                   Converts decimal to BCD
+ */
+uint8_t System::rtcDecToBcd(uint8_t val)
+{
+    return ((val / 10 * 16) + (val % 10));
+}
+
+/**
+ * @brief                   Converts BCD to decimal
+ */
+uint8_t System::rtcBcdToDec(uint8_t val)
+{
+    return ((val / 16 * 10) + (val % 16));
+}
+
+
+#endif
--- src/include/System.h
+++ src/include/System.h
@@ -27,6 +27,50 @@
 #include "Mcp.h"
 #include "NetworkClient.h"
 
+
+#ifdef ARDUINO_INKPLATE6PLUS
+
+#define I2C_ADDR 0x51
+
+// registar overview - crtl & status reg
+#define RTC_CTRL_1 0x0
+#define RTC_CTRL_2 0x01
+#define RTC_OFFSET 0x02
+#define RTC_RAM_by 0x03
+// registar overview - time & data reg
+#define RTC_SECOND_ADDR 0x04
+#define RTC_MINUTE_ADDR 0x05
+#define RTC_HOUR_ADDR   0x06
+#define RTC_DAY_ADDR    0x07
+#define RTC_WDAY_ADDR   0x08
+#define RTC_MONTH_ADDR  0x09
+// years 0-99; calculate real year = 1970 + RCC reg year
+#define RTC_YEAR_ADDR 0x0A
+// registar overview - alarm reg
+#define RTC_SECOND_ALARM 0x0B
+#define RTC_MINUTE_ALARM 0x0C
+#define RTC_HOUR_ALARM   0x0D
+#define RTC_DAY_ALARM    0x0E
+#define RTC_WDAY_ALARM   0x0F
+// registar overview - timer reg
+#define RTC_TIMER_VAL   0x10
+#define RTC_TIMER_MODE  0x11
+#define RTC_TIMER_TCF   0x08
+#define RTC_TIMER_TE    0x04
+#define RTC_TIMER_TIE   0x02
+#define RTC_TIMER_TI_TP 0x01
+// format
+// set AEN_x registers
+#define RTC_ALARM 0x80
+// set AIE ; enable/disable interrupt output pin
+#define RTC_ALARM_AIE 0x80
+// set AF register ; alarm flag needs to be cleared for alarm
+#define RTC_ALARM_AF       0x40
+#define RTC_CTRL_2_DEFAULT 0x00
+#define RTC_TIMER_FLAG     0x08
+
+#endif
+
 #ifdef ARDUINO_INKPLATE6PLUS
 #include "Frontlight.h"
 #include "Touch.h"
@@ -69,7 +113,69 @@ class System : public Esp,
         return _getRotation();
     };
 
+
+#ifdef ARDUINO_INKPLATE6PLUS
+
+    enum rtcCountdownSrcClock
+    {
+        TIMER_CLOCK_4096HZ = 0,
+        TIMER_CLOCK_64HZ = 1,
+        TIMER_CLOCK_1HZ = 2,
+        TIMER_CLOCK_1PER60HZ = 3
+    };
+
+    void rtcBegin();
+
+    void rtcSetTime(uint8_t hour, uint8_t minute, uint8_t sec);
+    void rtcSetDate(uint8_t weekday, uint8_t day, uint8_t month, uint16_t yr);
+    void rtcReadTime();
+    void rtcEnableAlarm(); // called on setAlarm()
+    void rtcSetAlarm(uint8_t alarm_second, uint8_t alarm_minute, uint8_t alarm_hour, uint8_t alarm_day,
+                     uint8_t alarm_weekday);
+    void rtcReadAlarm();
+    bool rtcChangeTimeFormat(); // returns false for 24H format & true for 12H format
+    void rtcTimerSet(rtcCountdownSrcClock source_clock, uint8_t value, bool int_enable, bool int_pulse);
+    bool rtcCheckTimerFlag();
+    void rtcReset();
+    /* read RTC times */
+    uint8_t rtcGetSecond();
+    uint8_t rtcGetMinute();
+    uint8_t rtcGetHour();
+    uint8_t rtcGetDay();
+    uint8_t rtcGetWeekday();
+    uint8_t rtcGetMonth();
+    uint16_t rtcGetYear();
+    /* read alarm times */ // if return = 99, no alarm
+    uint8_t rtcGetAlarmSecond();
+    uint8_t rtcGetAlarmMinute();
+    uint8_t rtcGetAlarmHour();
+    uint8_t rtcGetAlarmDay();
+    uint8_t rtcGetAlarmWeekday();
+
+#endif
+
   private:
+#ifdef ARDUINO_INKPLATE6PLUS
+    uint8_t rtcDecToBcd(uint8_t val);
+    uint8_t rtcBcdToDec(uint8_t val);
+    /* time variables */
+    uint8_t rtcHour;
+    uint8_t rtcMinute;
+    uint8_t rtcSecond;
+    uint8_t rtcDay;
+    uint8_t rtcWeekday;
+    uint8_t rtcMonth;
+    uint16_t rtcYear;
+    /* alarm */
+    uint8_t rtcAlarmSecond;
+    uint8_t rtcAlarmMinute;
+    uint8_t rtcAlarmHour;
+    uint8_t rtcAlarmDay;
+    uint8_t rtcAlarmWeekday;
+    /* support */
+    uint8_t rtcControl2;
+#endif
+
     uint8_t _panelOn = 0;
     int16_t _sdCardOk = 0;
 };
