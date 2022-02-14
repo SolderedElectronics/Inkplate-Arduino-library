37ea083cb86248188c398ae2013d3ca81f4e806a
Zvonimir Haramustek <nitko12@hotmail.com>
2022-02-14 08:31:29 +0100

    Update Inkplate_Wake_Up_On_Touchpads.ino

--- examples/Inkplate6/Advanced_Inkplate_Features/Inkplate_Wake_Up_On_Touchpads/Inkplate_Wake_Up_On_Touchpads.ino
+++ examples/Inkplate6/Advanced_Inkplate_Features/Inkplate_Wake_Up_On_Touchpads/Inkplate_Wake_Up_On_Touchpads.ino
@@ -4,9 +4,7 @@
    Select "Inkplate 6(ESP32)" from Tools -> Board menu.
    Don't have "Inkplate 6(ESP32)" option? Follow our tutorial and add it:
    https://e-radionica.com/en/blog/add-inkplate-6-to-arduino-ide/
-
    Here is shown how to use MCP and ESP interrupts to wake up the MCU from deepsleep when touchpad is pressed.
-
    Want to learn more about Inkplate? Visit www.inkplate.io
    Looking to get support? Write on our forums: http://forum.e-radionica.com/en/
    15 July 2020 by e-radionica.com
@@ -24,11 +22,10 @@
 // Time ESP32 will go to sleep (in seconds)
 #define TIME_TO_SLEEP 30
 
-// bitmask for GPIO_34 which is connected to MCP INTB
-#define TOUCHPAD_WAKE_MASK (int64_t(1) << GPIO_NUM_34)
-
 // Initiate Inkplate object
 Inkplate display(INKPLATE_1BIT);
+
+byte touchPadPin = PAD1;
 
 // Store int in rtc data, to remain persistent during deep sleep
 RTC_DATA_ATTR int bootCount = 0;
@@ -48,11 +45,12 @@ void setup()
     // Our function declared below
     displayInfo();
 
-    // Go to sleep for TIME_TO_SLEEP seconds
+    // Go to sleep for TIME_TO_SLEEP seconds, but also enable wake up from gpio 34
+    // Gpio 34 is where the mcp interrupt is connected, check
+    // https://github.com/e-radionicacom/Inkplate-6-hardware/blob/master/Schematics%2C%20Gerber%2C%20BOM/Inkplate6%20Schematics.pdf
+    // for more detail
     esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-
-    // enable wake from MCP port expander on gpio 34
-    esp_sleep_enable_ext1_wakeup(TOUCHPAD_WAKE_MASK, ESP_EXT1_WAKEUP_ANY_HIGH);
+    esp_sleep_enable_ext0_wakeup(GPIO_NUM_34, 1);
 
     // Go to sleep
     esp_deep_sleep_start();
