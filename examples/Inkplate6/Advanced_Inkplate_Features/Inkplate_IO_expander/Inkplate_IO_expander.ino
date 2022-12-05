ad6abf5c23dd3e6077666d14a7a0b450d3edc4ae
Borna Biro <43830307+BornaBiro@users.noreply.github.com>
2022-12-05 12:49:00 +0100

    Fixed IO Expander examples.

--- examples/Inkplate6/Advanced_Inkplate_Features/Inkplate_IO_expander/Inkplate_IO_expander.ino
+++ examples/Inkplate6/Advanced_Inkplate_Features/Inkplate_IO_expander/Inkplate_IO_expander.ino
@@ -5,16 +5,16 @@
    Don't have "Inkplate 6(ESP32)" option? Follow our tutorial and add it:
    https://e-radionica.com/en/blog/add-inkplate-6-to-arduino-ide/
 
-   Connect resistor to GPB7 pin on IO Expander header at bottom right corner on the backside (component side) of Inkplate.
-   You will have to connect one side of 330 Ohm resistor to GPB7, than other side to anode of LED and finally, cathode
-   pin of LED to GND.
-   
+   Connect resistor to P1-7 pin (GPB7 pin on older Inkplates with MCP23017) on IO Expander 2 header at bottom on the
+   backside (component side) of Inkplate. You will have to connect one side of 330 Ohm resistor to P1-7 (GPB7), then
+   other side to anode of LED and finally, cathode pin of LED to GND.
+
    This example will show you how you can manipulate with I/Os of external IO Expander.
    On this expander all pins are free to use, nothing is connected by the default.
 
    Want to learn more about Inkplate? Visit www.inkplate.io
    Looking to get support? Write on our forums: http://forum.e-radionica.com/en/
-   15 July 2020 by Soldered
+   5 December 2022 by Soldered Electronics.
 */
 
 // Next 3 lines are a precaution, you can ignore those, and the example would also work without them
@@ -24,8 +24,10 @@
 
 #include "Inkplate.h" //Include Inkplate library to the sketch
 
+// We are going to use pin P1-7 (or GPB7 on older Inkplates).
+// Remember! GPA0 = 0, GPA1 = 1, ..., GPA7 = 7, GPB0 = 8, GBP1 = 9, ..., GPB7 = 15.
+// And also P0-0 = GPA0, P0-1 = GPA1, ..., P0-7 = GPA7, P1-0 = GPB0, P1-1 = GPB1, ..., P1-7 = GPB7.
 #define LED_PIN IO_PIN_B7
-// We are going to use pin GPB7 (remember! GPA0 = 0, GPA1 = 1, ..., GPA7 = 7, GPB0 = 8, GBP1 = 9, ..., GPB7 = 15)
 
 Inkplate display(INKPLATE_1BIT); // Create an object on Inkplate library and also set library into 1-bit mode (BW)
 
@@ -34,17 +36,16 @@ void setup()
     display.begin(); // Init Inkplate library (you should call this function ONLY ONCE)
     display.clearDisplay();
     display.display();
-    display.pinModeIO(
-        LED_PIN,
-        OUTPUT); // Set pin 15 (or GPB7) to output. On that pin, we sholud connect LED with current limiting resistor
-                 // If we do not specify which IO expander we want to use, by the default external IO expander will be used of the one
-                 // with header named IO_expander_name-2
+    display.pinModeIO(LED_PIN,
+                      OUTPUT); // Set P1-7 (or GPB7) to output. On that pin, we sholud connect LED with current limiting
+                               // resistor If we do not specify which IO expander we want to use, by the default
+                               // external IO expander will be used of the one with header named IO Expander 2.
 }
 
 void loop()
 {
     display.digitalWriteIO(LED_PIN, LOW);  // Set output to low (LED does not light up)
-    delay(1000);                            // Wait for one second
+    delay(1000);                           // Wait for one second
     display.digitalWriteIO(LED_PIN, HIGH); // Set output to high (LED lights up)
-    delay(1000);                            // Wait for one second
+    delay(1000);                           // Wait for one second
 }
