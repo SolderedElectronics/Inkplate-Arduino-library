0a45130528991748f60065dfe0429b29422db75e
KarloLeksic <lekxka@gmail.com>
2023-01-23 11:54:26 +0100

    Added SD_TXT_WRITE example

--- examples/Inkplate10/Advanced_Inkplate_Features/Inkplate_SD_TXT_READ/Inkplate_SD_TXT_READ.ino
+++ examples/Inkplate10/Advanced_Inkplate_Features/Inkplate_SD_TXT_READ/Inkplate_SD_TXT_READ.ino
@@ -1,5 +1,5 @@
 /*
-   Inkplate_SD_TXT example for Soldered Inkplate 10
+   Inkplate_SD_TXT_READ example for Soldered Inkplate 10
    For this example you will need only a micro USB cable, Inkplate 10 and a SD card
    loaded with text.txt file that can be found inside folder of this example.
    Select "e-radionica Inkplate10" or "Soldered Inkplate10" from Tools -> Board menu.
