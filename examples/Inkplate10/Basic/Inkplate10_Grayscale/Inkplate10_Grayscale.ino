708c1243d9f30170dd0e91220e44143ee532d0ff
KarloLeksic <lekxka@gmail.com>
2023-02-20 08:00:25 +0100

    Corrected comment about changing display mode

--- examples/Inkplate10/Basic/Inkplate10_Grayscale/Inkplate10_Grayscale.ino
+++ examples/Inkplate10/Basic/Inkplate10_Grayscale/Inkplate10_Grayscale.ino
@@ -365,7 +365,7 @@ void loop()
     }
 
     // Did you know that you can change between BW and greyscale mode anytime?
-    // Just call Inkplate display(INKPLATE_1BIT) or Inkplate display(INKPLATE_3BIT)
+    // Just call display.setDisplayMode(mode)
 }
 
 // Small function that will write on the screen what function is currently in demonstration.
