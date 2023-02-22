6d810eb677d6f20565a48aa67edb558111083f9e
KarloLeksic <lekxka@gmail.com>
2023-02-22 11:54:57 +0100

    removed redundant SD pictures example for inkplate 10

--- examples/Inkplate10/Advance/SD/Inkplate10_SD_Pictures/Inkplate10_SD_Pictures.ino
+++ examples/Inkplate10/Advance/SD/Inkplate10_SD_Pictures/Inkplate10_SD_Pictures.ino
@@ -1,24 +1,21 @@
 /*
-   Inkplate10_SD_Pictures example for Soldered Inkplate 10
-   For this example you will need a micro USB cable, Inkplate6 and a SD card loaded with
-   image1.bmp and image2.bmp file that can be found inside folder of this example.
+   Inkplate10_SD_Pictures example for Soldered Inkplate10
+   For this example you will need a micro USB cable, Inkplate10 and a SD card loaded with
+   images that can be found inside folder of this example.
    Select "e-radionica Inkplate10" or "Soldered Inkplate10" from Tools -> Board menu.
    Don't have "e-radionica Inkplate10" or "Soldered Inkplate10" option? Follow our tutorial and add it:
    https://soldered.com/learn/add-inkplate-6-board-definition-to-arduino-ide/
 
-   To work with SD card on Inkplate, you will need to add one extra library.
-   Download and install it from here: https://github.com/e-radionicacom/Inkplate-6-SDFat-Arduino-Library
-
-   You can open .bmp, .jpeg or .png files that have color depth of 1 bit (BW bitmap), 4 bit, 8 bit and
-   24 bit AND have resoluton smaller than 1200x825 or otherwise it won't fit on screen.
-   Format your SD card in standard FAT fileformat.
+   You can open .bmp, .jpeg or .png files (but there are some limitations by the library) that have 
+   color depth of 1 bit (BW bitmap), 4 bit, 8 bit and 24 bit AND have resoluton smaller than 1024 x 758 
+   or otherwise it won't fit on screen. Format your SD card in standard FAT fileformat.
 
    This example will show you how you can read .bmp and .jpeg files (pictures) from SD card and
    display that image on e-paper display.
 
    Want to learn more about Inkplate? Visit www.inkplate.io
    Looking to get support? Write on our forums: https://forum.soldered.com/
-   11 February 2021 by Soldered
+   17 February 2023 by Soldered
 */
 
 // Next 3 lines are a precaution, you can ignore those, and the example would also work without them
@@ -26,25 +23,22 @@
 #error "Wrong board selection for this example, please select e-radionica Inkplate10 or Soldered Inkplate10 in the boards menu."
 #endif
 
-#include "Inkplate.h"            //Include Inkplate library to the sketch
-#include "SdFat.h"               //Include library for SD card
-Inkplate display(INKPLATE_3BIT); // Create an object on Inkplate library and also set library into 1 Bit mode (BW)
+#include "Inkplate.h"            // Include Inkplate library to the sketch
+Inkplate display(INKPLATE_3BIT); // Create an object on Inkplate library and also set library into 3 Bit mode
 SdFile file;                     // Create SdFile object used for accessing files on SD card
 
 void setup()
 {
-    Serial.begin(115200);
-
-    display.begin();        // Init Inkplate library (you should call this function ONLY ONCE)
-    display.clearDisplay(); // Clear frame buffer of display
-    display.display();      // Put clear image on display
-    display.setTextSize(5);
+    display.begin();             // Init Inkplate library (you should call this function ONLY ONCE)
+    display.clearDisplay();      // Clear frame buffer of display
+    display.setTextColor(BLACK); // Set text color to black
+    display.setTextSize(3);      // Set font size to 3
 
     // Init SD card. Display if SD card is init propery or not.
     if (display.sdCardInit())
     {
         display.println("SD Card OK! Reading image...");
-        display.partialUpdate();
+        display.display();
 
         // If card is properly init, try to load image and display it on e-paper at position X=0, Y=0
         // NOTE: Both drawImage methods allow for an optional fifth "invert" parameter. Setting this parameter
@@ -57,42 +51,43 @@ void setup()
             // compression! You can turn of dithering for somewhat faster image load by changing the last 1 to 0, or
             // removing the 1 argument completely
             display.println("Image open error");
-            display.display();
         }
+        display.display();
+        delay(5000);
+
+        // Now try to load image using SdFat library class (for more advanced users) and display image on epaper.
+        display.clearDisplay();
+        if (file.open("image2.bmp", O_RDONLY))
+        {
+            display.drawBitmapFromSd(&file, 0, 0);
+        }
+        else
+        {
+            display.println("Image open error");
+        }   
+        display.display();
+        delay(5000);
+    
+        // Now draw a JPEG
+        display.clearDisplay();
+        if (!display.drawImage("pyramid.jpg", 100, 0, true, false))
+        {
+            // If is something failed (wrong filename or wrong format), write error message on the screen.
+            // You can turn off dithering for somewhat faster image load by changing the fifth parameter to false, or
+            // removing the parameter completely
+            display.println("Image open error");
+        }       
         display.display();
     }
     else
     {
-        // If SD card init not success, display error on screen, put sd card in sleep mode, and stop the program (using infinite loop)
-        display.println("SD Card error!");
-        display.partialUpdate();
-        display.sdCardSleep();
-        while (true)
-            ;
-    }
-    delay(5000);
-
-    // Now try to load image using SdFat library class (for more advanced users) and display image on epaper.
-    if (file.open("image2.bmp", O_RDONLY))
-    {
-        display.drawBitmapFromSd(&file, 0, 0);
+        // If SD card init not success, display error on screen
+        display.println("SD Card error!");        
         display.display();
     }
 
-    display.clearDisplay();
-    delay(3000);
-
-    // Now draw a JPEG
-    if (!display.drawImage("pyramid.jpg", 100, 0, true, false))
-    {
-        // If is something failed (wrong filename or wrong format), write error message on the screen.
-        // You can turn off dithering for somewhat faster image load by changing the fifth parameter to false, or
-        // removing the parameter completely
-        display.println("Image open error");
-        display.display();
-    }
-    display.display();
-    display.sdCardSleep(); // Put sd card in sleep mode
+    // Turn off the MOSFET that powers the SD card
+    display.sdCardSleep();
 }
 
 void loop()
