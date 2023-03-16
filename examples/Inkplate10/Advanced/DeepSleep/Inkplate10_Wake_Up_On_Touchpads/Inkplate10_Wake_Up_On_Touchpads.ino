c5f1912bfd4d91d5aaf52547c174f4caae457db4
KarloLeksic <lekxka@gmail.com>
2023-03-16 08:47:56 +0100

    added new functionality to peripheral mode and fixed wake on touchpads on inkplate 10

--- examples/Inkplate10/Advanced/DeepSleep/Inkplate10_Wake_Up_On_Touchpads/Inkplate10_Wake_Up_On_Touchpads.ino
+++ examples/Inkplate10/Advanced/DeepSleep/Inkplate10_Wake_Up_On_Touchpads/Inkplate10_Wake_Up_On_Touchpads.ino
@@ -1,30 +1,34 @@
 /*
-   Inkplate10_Wake_Up_On_Touchpads example for Soldered Inkplate 10
-   For this example you will need USB cable and an Inkplate 10
-   Select "e-radionica Inkplate10" or "Soldered Inkplate10" from Tools -> Board menu.
-   Don't have "e-radionica Inkplate10" or "Soldered Inkplate10" option? Follow our tutorial and add it:
+   Inkplate10_Wake_Up_On_Touchpads example for e-radionica Inkplate 10 (older ones that have touchpads)
+   For this example you will need USB cable and an Inkplate 10.
+   Select "e-radionica Inkplate10" from Tools -> Board menu.
+   Don't have "e-radionica Inkplate10" option? Follow our tutorial and add it:
    https://soldered.com/learn/add-inkplate-6-board-definition-to-arduino-ide/
 
    Here is shown how to use I/O Expander and ESP interrupts to wake up the MCU from deepsleep when touchpad is pressed.
+   
+   NOTE: This is an example for the old Inkplates that have touchpads. 
+         If you select new Inkplate boards, you won't be able to compile the example.
 
    Want to learn more about Inkplate? Visit www.inkplate.io
    Looking to get support? Write on our forums: https://forum.soldered.com/
-   11 February 2021 by Soldered
+   15 March 2023 by Soldered
 */
 
 // Next 3 lines are a precaution, you can ignore those, and the example would also work without them
-#if !defined(ARDUINO_INKPLATE10) && !defined(ARDUINO_INKPLATE10V2)
-#error "Wrong board selection for this example, please select e-radionica Inkplate10 or Soldered Inkplate10 in the boards menu."
+#if !defined(ARDUINO_INKPLATE10)
+#error "Wrong board selection for this example, please select e-radionica Inkplate10 in the boards menu."
 #endif
 
 #include <Inkplate.h>
 
 // Conversion factor for micro seconds to seconds
 #define uS_TO_S_FACTOR 1000000
+
 // Time ESP32 will go to sleep (in seconds)
 #define TIME_TO_SLEEP 30
 
-// bitmask for GPIO_34 which is connected to I/O Expander INT pin
+// Bitmask for GPIO_34 which is connected to I/O Expander INT pin
 #define TOUCHPAD_WAKE_MASK (int64_t(1) << GPIO_NUM_34)
 
 // Initiate Inkplate object
@@ -35,20 +39,15 @@ RTC_DATA_ATTR int bootCount = 0;
 
 void setup()
 {
+    // Init serial communication and Inkplate library (you should call this functions only once)
     Serial.begin(115200);
     display.begin();
 
-// Different I/O expander has been used on Soldered Inkplate V2 board and original Soldered Inkplate.
-#ifdef ARDUINO_INKPLATE10
+    // Set interrupt pins
     display.setIntOutput(1, false, false, HIGH, IO_INT_ADDR);
     display.setIntPin(PAD1, RISING, IO_INT_ADDR);
     display.setIntPin(PAD2, RISING, IO_INT_ADDR);
     display.setIntPin(PAD3, RISING, IO_INT_ADDR);
-#elif ARDUINO_INKPLATE10V2
-    display.setIntPin(PAD1, IO_INT_ADDR);
-    display.setIntPin(PAD2, IO_INT_ADDR);
-    display.setIntPin(PAD3, IO_INT_ADDR);
-#endif
 
     ++bootCount;
 
@@ -58,16 +57,17 @@ void setup()
     // Go to sleep for TIME_TO_SLEEP seconds
     esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 
-    // Enable wakup from deep sleep on gpio 36
-    esp_sleep_enable_ext1_wakeup((1ULL << 36), ESP_EXT1_WAKEUP_ALL_LOW);
-
+    // Enable wakup from deep sleep on gpio 34
+    esp_sleep_enable_ext1_wakeup(TOUCHPAD_WAKE_MASK, ESP_EXT1_WAKEUP_ANY_HIGH);
+    
     // Go to sleep
     esp_deep_sleep_start();
 }
 
 void loop()
 {
-    // Never here
+    // Never here! If you are using deep sleep, the whole program should be in setup() because the board restarts each
+    // time. loop() must be empty!
 }
 
 // Function that will write number of boots and boot reason to screen
