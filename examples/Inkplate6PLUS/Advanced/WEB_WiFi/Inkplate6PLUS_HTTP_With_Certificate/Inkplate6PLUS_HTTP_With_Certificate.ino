15d45884c8ddd8d04c2eb7be7d426105842c6672
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2025-04-23 12:36:50 +0200

    Add examples for certificate validation

---
+++ examples/Inkplate6PLUS/Advanced/WEB_WiFi/Inkplate6PLUS_HTTP_With_Certificate/Inkplate6PLUS_HTTP_With_Certificate.ino
@@ -0,0 +1,115 @@
+/*
+   Inkplate6PLUS_Download_Files_With_Certificate example for Soldered Inkplate 6PLUS
+   For this example you will need a micro USB cable, Inkplate 6PLUS, and an available WiFi connection.
+   Select "Soldered Inkplate 6PLUS" from Tools -> Board menu.
+   Don't have "Soldered Inkplate 6PLUS" option? Follow our tutorial and add it:
+   https://soldered.com/learn/add-inkplate-6-board-definition-to-arduino-ide/
+
+   You can open .bmp files that have color depth of 1 bit (BW bitmap), 4 bit, 8 bit and
+   24 bit AND have resoluton smaller than 800x600 or otherwise it won't fit on screen.
+
+   This example will show you how you can download a .bmp file (picture) from the web securely by providing a 
+   certificate for the website that will be validated upon conncection and
+   display that image on e-paper display.
+
+   Want to learn more about Inkplate? Visit www.inkplate.io
+   Looking to get support? Write on our forums: https://forum.soldered.com/
+   15 March 2024 by Soldered
+*/
+
+// Next 3 lines are a precaution, you can ignore those, and the example would also work without them
+#if !defined(ARDUINO_INKPLATE6PLUS) && !defined(ARDUINO_INKPLATE6PLUSV2)
+#error "Wrong board selection for this example, please select e-radionica Inkplate 6Plus or Soldered Inkplate 6Plus in the boards menu."
+#endif
+
+#include "Inkplate.h"            //Include Inkplate library to the sketch
+Inkplate display(INKPLATE_1BIT); // Create an object on Inkplate library and also set library into 1 Bit mode (BW)
+
+const char ssid[] = "";    // Your WiFi SSID
+const char *password = ""; // Your WiFi password
+
+//This is the certificate for the website https://varipass.org
+//You can find information on how to get the certificate of a website
+//as well as how to format it here: https://randomnerdtutorials.com/esp32-https-requests/ 
+
+const char* certificate = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
+"-----END CERTIFICATE-----\n";
+
+void setup()
+{
+    display.begin();        // Init Inkplate library (you should call this function ONLY ONCE)
+    display.clearDisplay(); // Clear frame buffer of display
+    display.display();      // Put clear image on display
+
+    display.print("Connecting to WiFi...");
+    display.partialUpdate();
+    display.connectWiFi(ssid, password);
+    
+    while (!display.isConnected())
+    {
+        delay(500);
+        display.print(".");
+        display.partialUpdate();
+    }
+    display.println("\nWiFi OK! Downloading...");
+    display.partialUpdate();
+
+    //Apply the certificate previously defined
+    display.applyCertificate(certificate);
+    //Here we will draw the image using a valid certificate. Photo taken by: Roberto Fernandez
+    if (!display.drawImage("https://varipass.org/neowise_mono.bmp", 0, 0, false, true))
+    {
+        // If is something failed (wrong filename or wrong bitmap format), write error message on the screen.
+        // REMEMBER! You can only use Windows Bitmap file with color depth of 1, 4, 8 or 24 bits with no compression!
+        display.println("Image open error");
+        display.display();
+    }
+    display.display();
+    delay(3000);
+    display.clearDisplay();
+    //Next we will try to load an image from a different website, which will not work as the certificate is 
+    //not valid for this page
+    if (!display.drawImage("https://i.imgur.com/WdBS7WL.jpeg", 0, 100, true, false))
+    {
+        display.println("This image wont load as the certificate is invalid");
+        display.display();
+    }
+    display.display();
+
+    display.disconnect();
+}
+
+void loop()
+{
+    // Nothing...
+}
