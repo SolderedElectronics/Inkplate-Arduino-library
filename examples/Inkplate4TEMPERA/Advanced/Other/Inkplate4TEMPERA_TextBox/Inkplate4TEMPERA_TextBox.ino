28f31288405bb58d5e2cabb728969d4e640e6bcd
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2025-04-24 14:02:54 +0200

    Add examples for drawTextBox function

---
+++ examples/Inkplate4TEMPERA/Advanced/Other/Inkplate4TEMPERA_TextBox/Inkplate4TEMPERA_TextBox.ino
@@ -0,0 +1,56 @@
+/*
+   Inkplate4TEMPERA_TextBox example for Soldered Inkplate 4TEMPERA
+   For this example you will need a micro USB cable and an Inkplate 4TEMPERA
+   Select "Soldered Inkplate 4TEMPERA" from Tools -> Board menu.
+
+   This example will show you how to use the TextBox function with and without special parameters
+
+   Want to learn more about Inkplate? Visit www.inkplate.io
+   Looking to get support? Write on our forums: https://forum.soldered.com/
+   24 April 2025 by Soldered
+*/
+
+#include "Inkplate.h"            //Include Inkplate library to the sketch
+#include "Roboto_Light_36.h"
+Inkplate display(INKPLATE_1BIT); // Create an object on Inkplate library and also set library into 1 Bit mode (BW)
+
+// Define the text you will show in the text box
+const char* text="This is an example of a text written in a textbox. When a word doesn't fit into the current row, it goes to the next one."\
+" If the text reaches the lower bound, it ends with three dots (...) to mark that the text isnt displayed fully";
+
+void setup()
+{
+    display.begin();        // Init Inkplate library (you should call this function ONLY ONCE)
+    display.clearDisplay(); // Clear frame buffer of display
+    display.display();      // Put clear image on display
+
+    // Create a text box without any optional parameters
+    // x0- x coordinate of upper left corner
+    // y0- y coordinate of upper left corner
+    // x1- x coordinate of bottom right corner
+    // y1- y coordinate of bottom right corner
+    // text - text we want to display
+    display.drawTextBox(100,100,300,300,text);
+
+    // Create a text box with all parameters
+    // x0- x coordinate of upper left corner
+    // y0- y coordinate of upper left corner
+    // x1- x coordinate of bottom right corner
+    // y1- y coordinate of bottom right corner
+    // text - text we want to display
+    // textSizeMultiplier - by what factor we want to enlarge the size of a font
+    // font - address of selected custom font
+    // verticalSpacing - how many pixels between each row of text
+    // showBorder - Create a visible rectangle around the box
+    // fontSize - size of the used font in pt
+    int offset=32; // Note - some custom fonts are drawn from bottom-to-top which requires an offset, use an offset that best suits the font you use 
+    display.drawTextBox(400,100+offset,600,300,text,1,&Roboto_Light_36,27,false,36);
+
+    // Display both text boxes
+    display.display();
+}
+
+void loop()
+{
+    // Nothing...
+}
