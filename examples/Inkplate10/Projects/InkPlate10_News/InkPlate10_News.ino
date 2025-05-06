19fe34270276f9e6225bdec024dd47a0b5853fc8
Matej Andračić <93489900+luxzap7@users.noreply.github.com>
2025-05-06 09:38:12 +0200

    Inkplate10_News (New version completed)

--- examples/Inkplate10/Projects/InkPlate10_News/InkPlate10_News.ino
+++ examples/Inkplate10/Projects/InkPlate10_News/InkPlate10_News.ino
@@ -12,7 +12,7 @@
     - Inkplate documentation: https://www.inkplate.io
     - Support forums: https://forum.soldered.com/
 
-    Created by Soldered, 28 July 2020
+    Created by Soldered, 5.5.2025
 */
 
 // Ensure the correct board is selected in the Arduino IDE
@@ -24,37 +24,31 @@
 int timeZone = 2; // Adjust your timezone (e.g., 2 means UTC+2)
 
 // WiFi credentials
-char ssid[] = "YourWiFiSSID";
-char pass[] = "YourWiFiPassword";
-
-// News API key (get one from https://newsapi.org/)
-char api_key_news[] = "YourNewsAPIKey";
+char ssid[] = "Soldered-testingPurposes";
+char pass[] = "Testing443";
+char api_key_news[] = "10fb57f24c784ee7be7a9cc419b775cc";
 
 // ------------------------------------
 
 // Include necessary libraries
 #include "Inkplate.h"
-#include "Network.h"
+#include "src/Network.h"
 
 // Include necessary libraries for fonts
-#include "Inter12pt7b.h"
-#include "GT_Pressura16pt7b.h"
-#include "FreeSerifItalic24pt7b.h"
-#include "FreeSerifItalic12pt7b.h"
+#include "Fonts/Inter12pt7b.h"
+#include "Fonts/GT_Pressura16pt7b.h"
+#include "Fonts/FreeSerifItalic24pt7b.h"
 
 // Create network and display objects
 Network network;
-Inkplate display(INKPLATE_1BIT);
+Inkplate inkplate(INKPLATE_1BIT);
 
 // Constants for delays and refreshes
 #define DELAY_MS (uint32_t)60 * 60 * 1000 // Delay between API calls (1 hour)
 #define DELAY_WIFI_RETRY_SECONDS 10       // Delay for WiFi retry
 #define DELAY_API_RETRY_SECONDS 10        // Delay for API retry
-RTC_DATA_ATTR unsigned refreshes = 0;    // Counter for partial refreshes
-const int fullRefresh = 20;              // Full refresh after 20 partial refreshes
 
 // Function declarations
-bool checkIfAPIKeyIsValid(char *APIKEY);
 void setTime();
 void drawNews(struct news *entities);
 
@@ -64,80 +58,64 @@ void setup()
     Serial.begin(115200);
 
     // Initialize the display
-    display.begin();
-    display.setTextWrap(false);
+    inkplate.begin();
+    inkplate.setTextWrap(false);
 
     // Connect to WiFi
-    if (!display.connectWiFi(ssid, pass, WIFI_TIMEOUT, true))
-    {
-        // Display WiFi connection error
-        display.clearDisplay();
-        display.setTextSize(3);
-        display.setCursor((display.width() / 2) - 200, display.height() / 2);
-        display.print(F("Unable to connect to WiFi"));
-        display.setCursor((display.width() / 2) - 200, (display.height() / 2) + 30);
-        display.println(F("Check SSID and Password!"));
-        display.display();
-        esp_sleep_enable_timer_wakeup(1000L * DELAY_WIFI_RETRY_SECONDS);
-        (void)esp_deep_sleep_start();
-    }
-
-    // Validate the API key
-    if (!checkIfAPIKeyIsValid(api_key_news))
-    {
-        // Display API key error
-        display.clearDisplay();
-        display.setTextSize(3);
-        display.setCursor((display.width() / 2) - 200, display.height() / 2);
-        display.print(F("Invalid API Key!"));
-        display.setCursor((display.width() / 2) - 200, (display.height() / 2) + 30);
-        display.println(F("Check your API key."));
-        display.display();
-        esp_sleep_enable_timer_wakeup(1000L * DELAY_API_RETRY_SECONDS);
-        (void)esp_deep_sleep_start();
-    }
+    network.setCredentials(ssid, pass, api_key_news);
+    network.setTimeZone(timeZone);
+    network.begin();
 
     // Set the current time
     setTime();
 
     // Fetch news data and display it
-    struct news *entities = network.getData();
-    drawNews(entities);
+    struct news *entities = network.getData(inkplate);
+    if (entities != nullptr)
+    {
+        drawNews(entities);
+    }
+    else
+    {
+        inkplate.clearDisplay();
+        inkplate.setCursor(50, 230);
+        inkplate.setTextSize(2);
+        inkplate.println(F("Failed to fetch news"));
+        inkplate.display();
+    }
 
     // Update the display
-    display.display();
-
-    // Increment refresh counter
-    ++refreshes;
+    inkplate.display();
 
     // Enter deep sleep until the next update
     esp_sleep_enable_timer_wakeup(1000 * DELAY_MS);
-    (void)esp_deep_sleep_start();
+    esp_deep_sleep_start();
 }
 
+// Function to draw news items on the display
 void drawNews(struct news *entities)
 {
-    display.setRotation(3);
+    inkplate.setRotation(3);
 
     // Display the title "World News"
-    display.setFont(&FreeSerifItalic24pt7b);
+    inkplate.setFont(&FreeSerifItalic24pt7b);
     int textWidth = strlen("World News") * 12; // Approximate width (12 pixels per character)
-    int centerX = (display.width() - textWidth) / 2;
-    display.setCursor(centerX-70, 60); // -70 to ajust text position to center
-    display.print("World News");
+    int centerX = (inkplate.width() - textWidth) / 2;
+    inkplate.setCursor(centerX - 70, 60); // Adjust text position to center
+    inkplate.print("World News");
 
     // Draw a dividing line below the title
-    int xStart = display.width() * 0.03;
-    int xEnd = display.width() * 0.97;
+    int xStart = inkplate.width() * 0.03;
+    int xEnd = inkplate.width() * 0.97;
     for (int lineY = 80; lineY < 83; lineY++)
     {
-        display.drawLine(xStart, lineY, xEnd, lineY, BLACK);
+        inkplate.drawLine(xStart, lineY, xEnd, lineY, BLACK);
     }
 
     // Display the current date and time
     struct tm timeInfo;
     time_t nowSec;
-    display.getNTPEpoch(&nowSec);
+    inkplate.getNTPEpoch(&nowSec);
     while (nowSec < 8 * 3600 * 2)
     {
         delay(500);
@@ -146,7 +124,7 @@ void drawNews(struct news *entities)
     }
     gmtime_r(&nowSec, &timeInfo);
 
-        // Prepare date and time strings
+    // Prepare date and time strings
     char dateStr[20];
     char updateStr[20];
     sprintf(dateStr, "Date : %02d.%02d.%04d",
@@ -154,52 +132,52 @@ void drawNews(struct news *entities)
             timeInfo.tm_mon + 1,
             timeInfo.tm_year + 1900);
 
-    sprintf(updateStr, "Last update : %02d.%02d",
+    sprintf(updateStr, "Last update : %02d:%02d",
             timeInfo.tm_hour,
             timeInfo.tm_min);
 
     // Choose font
-    display.setFont(&Inter12pt7b);
+    inkplate.setFont(&Inter12pt7b);
 
     // Y position for the row
     int yPos = 105;
 
     // Print date left-aligned
-    display.setCursor(23, yPos); // 23 px from left
-    display.print(dateStr);
+    inkplate.setCursor(23, yPos); // 23 px from left
+    inkplate.print(dateStr);
 
     // Calculate width of 'Last update' string for right alignment
-    int updateStrWidth = strlen(updateStr) * 12; // adjust 12 for your font's avg char width
-    int xRight = display.width() - updateStrWidth - 19; // 19 px margin from right
+    int updateStrWidth = strlen(updateStr) * 12; // Adjust 12 for your font's avg char width
+    int xRight = inkplate.width() - updateStrWidth - 19; // 19 px margin from right
 
     // Print 'Last update' right-aligned
-    display.setCursor(xRight, yPos);
-    display.print(updateStr);
+    inkplate.setCursor(xRight, yPos);
+    inkplate.print(updateStr);
 
     // Draw a line below the date
     for (int lineY = 113; lineY < 116; lineY++)
     {
-        display.drawLine(xStart, lineY, xEnd, lineY, BLACK);
+        inkplate.drawLine(xStart, lineY, xEnd, lineY, BLACK);
     }
 
     // Render news items
-    int startY = 200;
+    int startY = 160;
     int boxHeight = 170;
-    int boxSpacing = 20;
+    int boxSpacing = 35;
     int leftMargin = 20;
     int rightMargin = 20;
-    int maxBoxes = (display.height() - startY) / (boxHeight + boxSpacing);
+    int maxBoxes = (inkplate.height() - startY) / (boxHeight + boxSpacing);
 
-    for (int i = 0; i < maxBoxes && entities[i].title != NULL && entities[i].description != NULL; i++)
+    for (int i = 0; i < maxBoxes && entities[i].title[0] != '\0' && entities[i].description[0] != '\0'; i++)
     {
         int y0 = startY + i * (boxHeight + boxSpacing);
         int y1 = y0 + boxHeight;
 
         // Draw the title
-        display.drawTextBox(leftMargin, y0, display.width() - rightMargin, y0 + 80, entities[i].title, 1, &GT_Pressura16pt7b, 30, false, 18);
+        inkplate.drawTextBox(leftMargin, y0+5, inkplate.width() - rightMargin, y0 + 80, entities[i].title, 1, &GT_Pressura16pt7b, 30, false, 18);
 
         // Draw the description
-        display.drawTextBox(leftMargin, y0 + 80, display.width() - rightMargin, y1 + 20, entities[i].description, 1, &Inter12pt7b, 30, false, 16);
+        inkplate.drawTextBox(leftMargin, y0 + 80, inkplate.width() - rightMargin, y1 + 20, entities[i].description, 1, &Inter12pt7b, 30, false, 16);
     }
 }
 
@@ -207,7 +185,7 @@ void setTime()
 {
     struct tm timeInfo;
     time_t nowSec;
-    display.getNTPEpoch(&nowSec);
+    inkplate.getNTPEpoch(&nowSec);
     while (nowSec < 8 * 3600 * 2)
     {
         delay(500);
@@ -217,38 +195,6 @@ void setTime()
     gmtime_r(&nowSec, &timeInfo);
     Serial.print(F("Current time: "));
     Serial.print(asctime(&timeInfo));
-}
-
-bool checkIfAPIKeyIsValid(char *APIKEY)
-{
-    char *data = (char *)ps_malloc(50000LL);
-    HTTPClient http;
-    http.getStream().setTimeout(10);
-    http.getStream().flush();
-    http.getStream().setNoDelay(true);
-
-    char *baseURL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
-    char apiTestURL[200];
-    sprintf(apiTestURL, "%s%s", baseURL, APIKEY);
-
-    http.begin(apiTestURL);
-    delay(300);
-
-    int httpCode = http.GET();
-    bool failed = (httpCode != 200);
-
-    if (failed)
-    {
-        Serial.println("Invalid API key.");
-    }
-    else
-    {
-        Serial.println("API key is valid.");
-    }
-
-    http.end();
-    free(data);
-    return !failed;
 }
 
 void loop()
---
+++ examples/Inkplate10/Projects/Inkplate10_News/src/Network.cpp
@@ -0,0 +1,182 @@
+/*
+    Network.cpp
+    Inkplate 10 Arduino library
+    Matej Andračić @ Soldered
+    May 5, 2025
+    https://github.com/SolderedElectronics/Inkplate-Arduino-library/tree/master/examples/Inkplate10
+
+    For support, please reach over forums: forum.e-radionica.com/en
+    For more info about the product, please check: www.inkplate.io
+
+    This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
+    Please review the LICENSE file included with this example.
+    If you have any questions about licensing, please contact techsupport@e-radionica.com
+    Distributed as-is; no warranty is given.
+*/
+
+#include "Network.h"
+#include "Inkplate.h"
+
+// Must be installed for this example to work
+#include <ArduinoJson.h>
+
+// Static Json from ArduinoJson library
+StaticJsonDocument<35000> doc;
+
+void Network::setCredentials(const char *wifiSSID, const char *wifiPass, const char *apiKey)
+{
+    strncpy(ssid, wifiSSID, sizeof(ssid) - 1);
+    strncpy(pass, wifiPass, sizeof(pass) - 1);
+    strncpy(api_key_news, apiKey, sizeof(api_key_news) - 1);
+}
+
+void Network::setTimeZone(int tz)
+{
+    timeZone = tz;
+}
+
+void Network::begin()
+{
+    WiFi.begin(ssid, pass);
+    Serial.println(F("Connecting to WiFi..."));
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.print(F("."));
+    }
+
+    Serial.println(F("\nWiFi connected."));
+}
+
+struct news* Network::getData(Inkplate &inkplate)
+{
+    struct news *ent = nullptr;
+
+    // If not connected to WiFi, reconnect,
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        WiFi.reconnect();
+        delay(5000);
+
+        int cnt = 0;
+        Serial.println(F("Waiting for WiFi to reconnect..."));
+        while (WiFi.status() != WL_CONNECTED)
+        {
+            Serial.print(F("."));
+            delay(1000);
+            ++cnt;
+
+            if (cnt == 7)
+            {
+                Serial.println("Can't connect to WiFi, restarting...");
+                delay(100);
+                ESP.restart();
+            }
+        }
+    }
+
+    // Disable WiFi sleep mode temporarily
+    bool sleep = WiFi.getSleep();
+    WiFi.setSleep(false);
+
+    // HTTP object for GET request
+    HTTPClient http;
+    http.getStream().setTimeout(10);
+    http.getStream().flush();
+
+    // Prepare the API URL
+    char temp[128];
+    sprintf(temp, "https://newsapi.org/v2/top-headlines?country=us&apiKey=%s", this->api_key_news);
+    http.begin(temp);
+
+    // Perform the GET request
+    int httpCode = http.GET();
+    if (httpCode == 200)
+    {
+        while (http.getStream().available() && http.getStream().peek() != '{')
+        {
+            (void)http.getStream().read();
+        }
+
+        // Parse the JSON response
+        DeserializationError error = deserializeJson(doc, http.getStream());
+        if (error)
+        {
+            Serial.print(F("deserializeJson() failed: "));
+            Serial.println(error.c_str());
+            return nullptr;
+        }
+
+        if (doc["status"])
+        {
+            int n = doc["articles"].size();
+            Serial.printf("Number of articles: %d\n", n);
+
+            // Allocate memory for the news array
+            ent = (struct news *)ps_malloc(n * sizeof(struct news));
+            if (!ent)
+            {
+                Serial.println(F("Memory allocation failed!"));
+                return nullptr;
+            }
+
+            int i = 0;
+            for (JsonObject article : doc["articles"].as<JsonArray>())
+            {
+                const char *temp_title = article["title"];
+                const char *temp_description = article["description"];
+
+                // Allocate memory for title and copy the string
+                if (temp_title)
+                {
+                    size_t title_len = strlen(temp_title) + 1; // Include null terminator
+                    ent[i].title = (char *)malloc(title_len);
+                    if (ent[i].title)
+                        memcpy(ent[i].title, temp_title, title_len);
+                }
+                else
+                {
+                    ent[i].title = nullptr;
+                }
+
+                // Allocate memory for description and copy the string
+                if (temp_description)
+                {
+                    size_t description_len = strlen(temp_description) + 1; // Include null terminator
+                    ent[i].description = (char *)malloc(description_len);
+                    if (ent[i].description)
+                        memcpy(ent[i].description, temp_description, description_len);
+                }
+                else
+                {
+                    ent[i].description = nullptr;
+                }
+
+                i++;
+            }
+        }
+    }
+    else if (httpCode == 404)
+    {
+        // Handle case where no news is found
+        inkplate.clearDisplay();
+        inkplate.setCursor(50, 230);
+        inkplate.setTextSize(2);
+        inkplate.println(F("No news"));
+        inkplate.display();
+        while (1)
+            ;
+    }
+
+    // Clear the JSON document and end the HTTP request
+    doc.clear();
+    http.end();
+
+    // Restore WiFi sleep mode
+    WiFi.setSleep(sleep);
+
+    return ent;
+}
+
+
