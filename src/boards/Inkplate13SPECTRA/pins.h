2e9988a3d1c6e292ac4b471815168e0e6ff80cb1
Clang Robot <robot@example.com>
2026-08-25 05:46:19 +0000

    Committing clang-format changes

--- src/boards/Inkplate13SPECTRA/pins.h
+++ src/boards/Inkplate13SPECTRA/pins.h
@@ -34,8 +34,7 @@ static uint32_t pallete[] = {0x000000, 0xFFFFFF, 0xFFFF00, 0xFF0000, 0x0000FF, 0
 #define INKPLATE_BLUE   4
 #define INKPLATE_GREEN  5
 
-static uint16_t colorPalette[6] = {INKPLATE_BLACK, INKPLATE_WHITE, INKPLATE_YELLOW,
-                                   INKPLATE_RED,   0x05,  0x06};
+static uint16_t colorPalette[6] = {INKPLATE_BLACK, INKPLATE_WHITE, INKPLATE_YELLOW, INKPLATE_RED, 0x05, 0x06};
 
 
 // Screen resolution.
